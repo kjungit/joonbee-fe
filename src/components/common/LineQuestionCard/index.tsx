b6cafe6633d8ffbdf4de291ebb02e2bc5ad49f96
kjun <kjun_all@naver.com>
2023-10-23 02:02:30 +0900

    KAN-15 LineQuestionCard 컴포넌트 구현

---
+++ src/components/common/LineQuestionCard/index.tsx
@@ -0,0 +1,61 @@
+'use client';
+import React from 'react';
+
+type CardSize = 'sm' | 'md' | 'lg' | 'xl';
+type CardColor = 'white' | 'gray';
+type FontSize = 'md' | 'lg';
+type BorderPosition = 'left' | 'bottom';
+
+export interface LineQuestrionCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  size?: CardSize;
+  color?: CardColor;
+  fontSize?: FontSize;
+  borderPosition?: BorderPosition;
+}
+
+export const LineQuestrionCard = ({
+  color = 'white',
+  size = 'sm',
+  fontSize = 'md',
+  borderPosition = 'left',
+  children,
+  ...props
+}: LineQuestrionCardProps) => {
+  const baseStyles = 'flex p-6 items-center  text-ellipsis  font-bold shadow-md rounded-xl';
+
+  const sizeStyles = {
+    sm: 'h-[54px] w-[740px] overflow-auto',
+    md: 'h-[104px] w-[584px] overflow-auto',
+    lg: ' w-[1040px]',
+    xl: ' w-[500px]',
+  };
+
+  const colorStyles = {
+    white: 'bg-white text-main-primary',
+    gray: `bg-gray-normal text-main-primary`,
+  };
+
+  const fontStyles = {
+    md: 'text-[16px]',
+    lg: `text-[20px]`,
+  };
+
+  const borderPositionStyles = {
+    left: 'border-l-main-primary border-l-[12px]',
+    bottom: `border-b-main-primary border-b-[12px]`,
+  };
+
+  const buttonStyles = `
+  ${baseStyles}
+  ${sizeStyles[size]}
+  ${colorStyles[color]}
+  ${fontStyles[fontSize]}
+  ${borderPositionStyles[borderPosition]}
+  `;
+
+  return (
+    <div className={buttonStyles} {...props}>
+      <p>{children}</p>
+    </div>
+  );
+};
