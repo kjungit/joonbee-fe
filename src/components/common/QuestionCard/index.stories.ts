11473bf0e101781021debde4be075d03e63b14aa
kjun <kjun_all@naver.com>
2023-10-23 00:49:40 +0900

    KAN-14 QuestionCard 컴포넌트 구현

---
+++ src/components/common/QuestionCard/index.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { QuestrionCard } from '.';
+
+const meta: Meta<typeof QuestrionCard> = {
+  title: 'atoms/QuestrionCard',
+  component: QuestrionCard,
+  argTypes: {
+    color: {
+      description: 'The type of button',
+      control: 'inline-radio',
+      options: ['white', 'gray', 'navy'],
+    },
+    size: {
+      description: 'The type of button',
+      control: 'inline-radio',
+      options: ['sm', 'md', 'lg'],
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof QuestrionCard>;
+
+export const Primary: Story = {
+  args: {
+    size: `sm`,
+    color: 'white',
+    children: 'react에 대해서 설명해주세요.',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    size: `md`,
+    color: 'gray',
+    children: 'react에 대해서 설명해주세요.',
+  },
+};
+
+export const Normal: Story = {
+  args: {
+    size: `lg`,
+    color: 'white',
+    children: 'react에 대해서 설명해주세요.',
+  },
+};
---
+++ src/components/common/QuestionCard/index.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React from 'react';
+
+type CardSize = 'sm' | 'md' | 'lg';
+type CardColor = 'white' | 'gray' | 'navy';
+
+export interface QuestrionCardProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: CardSize;
+  color?: CardColor;
+}
+
+export const QuestrionCard = ({
+  color = 'white',
+  size = 'sm',
+  children,
+  ...props
+}: QuestrionCardProps) => {
+  const baseStyles = 'flex items-center font-bold pl-5 shadow-md rounded-xl';
+
+  const sizeStyles = {
+    sm: 'h-[42px] w-[354px] text-[14px]',
+    md: 'h-[52px] w-[584px] ',
+    lg: 'h-[68px] w-[556px]',
+  };
+
+  const colorStyles = {
+    white: 'bg-white text-main-primary',
+    gray: `bg-gray-normal text-main-primary`,
+    navy: 'bg-main-primary text-gray-normal',
+  };
+
+  const buttonStyles = `${baseStyles} ${sizeStyles[size]} ${colorStyles[color]}`;
+
+  return (
+    <button className={buttonStyles} {...props}>
+      {children}
+    </button>
+  );
+};
