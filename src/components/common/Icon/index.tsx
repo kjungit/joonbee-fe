c34e45801a8a5c6df8e5dd7bac49bccecc019739
dokimion24 <leecy3610@gmail.com>
2023-10-24 19:17:55 +0900

    KAN-28 Feat: Icon 스토리 컴포넌트 구현

---
+++ src/components/common/Icon/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+type IconName =
+  | 'kakao'
+  | 'naver'
+  | 'google'
+  | 'meeting'
+  | 'service'
+  | 'emptylike'
+  | 'copy'
+  | 'check'
+  | 'random'
+  | 'blank'
+  | 'ai_white'
+  | 'checklist';
+
+export interface IconProps {
+  name: IconName;
+}
+
+export const Icon = ({ name }: IconProps) => {
+  const icon = require(`/public/icons/${name}.png`);
+
+  return <Image src={icon} alt={icon} />;
+};
