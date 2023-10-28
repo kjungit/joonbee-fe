464bf9733e0089230b22f5fb3fb57fd491a93d34
lcy <lcy@lcys-MacBook-Pro.local>
2023-10-27 20:08:25 -0700

    KAN-33 Feat: Dropdwon.List 컴포넌트 작성

---
+++ src/components/common/Dropdown/List.tsx
@@ -0,0 +1,36 @@
+import { selectedValueState } from '../../../recoil/select/atom';
+import { useRecoilState } from 'recoil';
+
+type ListProps = {
+  children: React.ReactNode;
+  name: 'category' | 'subcategory';
+  size: 'sm' | 'md';
+};
+
+const List = ({ children, name, size = 'sm' }: ListProps) => {
+  const baseStyles = `bg-main-primary text-white rounded-[8px] font-bold text-center hover:bg-hover-primary mr-[20px]`;
+  const sizeStyles = {
+    sm: 'text-[16px] w-[116px] h-[44px]',
+    md: 'text-[20px] w-[160px] h-[60px]',
+  };
+
+  const ListStyles = `${baseStyles} ${sizeStyles[size]}`;
+
+  const [selectedValue, setSelectedValue] = useRecoilState(selectedValueState);
+
+  const onChangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    name === 'category'
+      ? setSelectedValue(prev => ({ ...prev, selectedCategory: e.target.value }))
+      : setSelectedValue(prev => ({ ...prev, selectedSubcategory: e.target.value }));
+  };
+
+  return (
+    <label>
+      <select name={name} defaultValue="카테고리" onChange={onChangeSelect} className={ListStyles}>
+        {children}
+      </select>
+    </label>
+  );
+};
+
+export default List;
