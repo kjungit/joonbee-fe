75f61a9da5a0e0b3aa124c47b5093c0f48deec7c
lcy <lcy@lcys-MacBook-Pro.local>
2023-11-01 15:16:01 +0900

    Feat: import 경로 수정

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,4 +1,4 @@
-import { IconName } from '@/components/common/IconButton';
+import { IconName } from '@/components/common/SocialLoginButton';
 
 export const capitalizeFirstLetter = (text: IconName) => {
   return text.charAt(0).toUpperCase() + text.slice(1);
