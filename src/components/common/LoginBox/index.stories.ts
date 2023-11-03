f8cf023714f3d432481bad676f93c38aa6cfa0a9
kjun <kjun_all@naver.com>
2023-11-04 02:31:29 +0900

    KAN-48 Feat: LoginBox 컴포넌트 구현

--- src/components/common/LoginBox/index.stories.ts
+++ src/components/common/LoginBox/index.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { LoginBox } from '.';
 
 const meta: Meta<typeof LoginBox> = {
-  title: 'DESIGN SYSTEM/Atoms/LoginBox',
+  title: 'DESIGN SYSTEM/Molecules/LoginBox',
   component: LoginBox,
   tags: ['autodocs'],
 };
--- src/components/common/LoginBox/index.tsx
+++ src/components/common/LoginBox/index.tsx
@@ -1,14 +1,20 @@
 'use client';
-import Image from 'next/image';
 import React from 'react';
+import Logo from '../Logo';
+import { SocialLoginButton } from '../SocialLoginButton';
 
 export const LoginBox = () => {
-  const image = require(`/public/icons/logo.png`);
-
   return (
-    <div className="w-[410px] h-[580px] rounded-[50px] bg-white shadow-md">
-      <Image src={image} width={120} height={120} alt={`로고`} />
-      <p>JOONBEE 하세요.</p>
+    <div className="w-[410px] h-[580px] rounded-[50px] bg-white shadow-md flex items-center justify-center">
+      <div className="flex items-center flex-col">
+        <Logo size={'md'} />
+        <p className="text-[30px] mt-7 mb-10 font-bold text-blue-secondary">JOONBEE 하세요.</p>
+        <div className="flex flex-col gap-3">
+          <SocialLoginButton name="kakao" onClick={() => {}} />
+          <SocialLoginButton name="naver" onClick={() => {}} />
+          <SocialLoginButton name="google" onClick={() => {}} />
+        </div>
+      </div>
     </div>
   );
 };
