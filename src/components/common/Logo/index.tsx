75290a84ac7c62cabda47e557ebb8962cbf12d91
kjun <kjun_all@naver.com>
2023-11-04 02:29:59 +0900

    Feat: Logo 컴포넌트 props, style 코드 수정

--- src/components/common/Logo/index.tsx
+++ src/components/common/Logo/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-
 import Image from 'next/image';
 
-export interface IconProps {
-  size: number;
-}
-const Logo = ({ size = 30 }: IconProps) => {
+type IconProps = {
+  size: 'sm' | 'md';
+};
+
+const Logo = ({ size = 'sm' }: IconProps) => {
   const logo = require(`/public/icons/logo.png`);
 
-  return <Image src={logo} alt="JOONBEE Logo" width={size} height={size} />;
+  const textStyles = {
+    sm: 29,
+    md: 120,
+  };
+
+  return <Image src={logo} alt="JOONBEE Logo" width={textStyles[size]} height={textStyles[size]} />;
 };
 
 export default Logo;
