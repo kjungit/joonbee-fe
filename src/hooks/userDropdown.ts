32bd1af4260cba35116c8024ce24be5afeab9b54
dokimion24 <leecy3610@gmail.com>
2023-11-04 15:51:23 +0900

    Feat: Dropdown에서 사용되는 함수useDropdown 생성

---
+++ src/hooks/userDropdown.ts
@@ -0,0 +1,29 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+const useDropdown = () => {
+  const [isOpened, setIsOpened] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  console.log(dropdownRef.current);
+
+  const toggleDropdown = () => {
+    setIsOpened(prev => !prev);
+  };
+
+  const handleDocumentClick = (e: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
+      setIsOpened(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleDocumentClick);
+    return () => {
+      document.removeEventListener('mousedown', handleDocumentClick);
+    };
+  }, []);
+
+  return { isOpened, toggleDropdown, dropdownRef };
+};
+
+export default useDropdown;
