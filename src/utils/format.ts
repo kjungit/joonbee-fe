ef8907a64d149244459a1adcd32bb3ca3ac8690b
dokimion24 <leecy3610@gmail.com>
2023-11-04 15:52:05 +0900

    Feat: maskNickname 함수 작성

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -3,3 +3,13 @@ import { IconName } from '@/components/common/SocialLoginButton';
 export const capitalizeFirstLetter = (text: IconName) => {
   return text.charAt(0).toUpperCase() + text.slice(1);
 };
+
+export const maskNickname = (nickname: any) => {
+  if (nickname.length <= 3) {
+    return nickname;
+  }
+
+  const visibleText = nickname.substring(0, 3);
+  const maskedText = '*****';
+  return visibleText + maskedText;
+};
