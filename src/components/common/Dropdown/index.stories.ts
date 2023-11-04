1eaf9225e1849bf126427b0187a187c883d8d528
dokimion24 <leecy3610@gmail.com>
2023-11-04 22:52:48 +0900

    Feat: story docs 높이 수정

--- src/components/common/Dropdown/index.stories.ts
+++ src/components/common/Dropdown/index.stories.ts
@@ -6,6 +6,14 @@ const meta: Meta<typeof Dropdown> = {
   title: 'Design System/Molecules/Dropdown',
   component: Dropdown,
   tags: ['autodocs'],
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 400,
+      },
+    },
+  },
   argTypes: {
     size: {
       control: 'inline-radio',
@@ -21,7 +29,7 @@ type Story = StoryObj<typeof Dropdown>;
 export const Primary: Story = {
   args: {
     size: 'md',
-    data: ['프론트엔드', '백엔드', '언어', 'CS', '모바일','기타'],
+    data: ['프론트엔드', '백엔드', '언어', 'CS', '모바일', '기타'],
     title: '카테고리',
   },
 };
