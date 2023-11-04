e41803d322cbb93e6178f2fcfaa84cc8c7bad36b
lcy <lcy@lcys-MacBook-Pro.local>
2023-11-04 10:10:57 +0900

    Feat: isAlaram props 제거

--- src/components/common/Avartar/index.tsx
+++ src/components/common/Avartar/index.tsx
@@ -8,11 +8,10 @@ export interface AvatarProps {
   size: AvatarSize;
   children: React.ReactNode;
   onClick: () => void;
-  isAlarm: boolean;
   profile: string;
 }
 
-export const Avatar = ({ size, isAlarm = false, profile, onClick }: AvatarProps) => {
+export const Avatar = ({ size, profile, onClick }: AvatarProps) => {
   const baseStyles = 'rounded-full bg-gray-normal relative';
 
   const sizeStyles = {
@@ -21,15 +20,20 @@ export const Avatar = ({ size, isAlarm = false, profile, onClick }: AvatarProps)
     lg: 'w-[140px] h-[140px]',
   };
 
-  const alarmStyles = `absolute w-[10px] h-[10px] bg-status-alert rounded-full right-0 top-[1px]`;
+  // const alarmStyles = `absolute w-[10px] h-[10px] bg-status-alert rounded-full right-0 top-[1px]`;
   const AvatarStyles = `${baseStyles} ${sizeStyles[size]}`;
 
   return (
     <div className={AvatarStyles} onClick={onClick}>
       {profile && (
-        <Image className="rounded-full" src={naver} alt="naver" layout="fill" objectFit="cover" />
+        <Image
+          className="rounded-full"
+          src={profile}
+          alt="profile"
+          layout="fill"
+          objectFit="cover"
+        />
       )}
-      {isAlarm && <div className={alarmStyles} />}
     </div>
   );
 };
