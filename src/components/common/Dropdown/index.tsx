644fac4eef1d3e19adbffc4f879d901c24d217f1
dokimion24 <leecy3610@gmail.com>
2023-11-04 22:38:35 +0900

    Feat: key props 추가, design 수정

--- src/components/common/Dropdown/index.tsx
+++ src/components/common/Dropdown/index.tsx
@@ -7,15 +7,17 @@ type DropdownProps = {
   size?: 'sm' | 'md';
   data: string[];
   title?: string;
+  onSelect: (item: string) => void;
 };
 
-const Dropdown = ({ size = 'md', data, title = '카테고리' }: DropdownProps) => {
+const Dropdown = ({ size = 'md', data, title = '카테고리', onSelect }: DropdownProps) => {
   const [selected, setSelected] = useState('');
   const { isOpened, toggleDropdown, dropdownRef } = useDropdown();
 
   const onSelectItem = (item: string) => {
     setSelected(item);
     onToggleList();
+    onSelect(item);
   };
 
   const onToggleList = () => {
@@ -33,7 +35,7 @@ const Dropdown = ({ size = 'md', data, title = '카테고리' }: DropdownProps) 
       list: 'top-[50px] h-[178px]',
     },
     md: {
-      item: 'px-[26px] py-[12px] text-[20px]',
+      item: 'px-[22px] py-[12px] text-[20px]',
       section: 'w-[160px]',
       list: 'top-[70px] h-[234px] w-[160px]',
     },
@@ -46,21 +48,20 @@ const Dropdown = ({ size = 'md', data, title = '카테고리' }: DropdownProps) 
       </Button>
       {isOpened && (
         <ul
-          className={`shadow-md px-[8px] py-[6px] rounded-[8px] overflow-y-scroll absolute
+          className={`shadow-md px-[8px] py-[6px] rounded-[8px] overflow-y-scroll scroll-hide  absolute
           ${sizeStyles[size].list}`}>
           {data.map(item => (
-            <>
+            <div key={item}>
               <li
                 className={`font-bold hover:bg-hover-grayLight hover:shadow-normal rounded-[8px] text-center
                 text-[#444] ${sizeStyles[size].item} ${
                   item === selected ? 'bg-hover-grayLight shadow-noraml' : ''
                 }`}
-                key={item}
                 onClick={() => onSelectItem(item)}>
                 {item}
               </li>
               <div className="border-b-2 border-gray-normal" />
-            </>
+            </div>
           ))}
         </ul>
       )}
