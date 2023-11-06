0db826a827f677020f7d8942544406054e27cbf5
dokimion24 <leecy3610@gmail.com>
2023-11-06 19:28:15 +0900

    Feat: use client 추가

--- src/components/common/Alarm/index.tsx
+++ src/components/common/Alarm/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState } from 'react';
 import { VariableIcon } from '../../ui/VariableIcon';
 import useModalOutsideClick from '../../../hooks/useModalOutsideClick';
