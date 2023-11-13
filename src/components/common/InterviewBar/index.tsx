40ea2b0357879cb39145ecb74ed96cd6b2b29480
dokimion24 <leecy3610@gmail.com>
2023-11-13 16:10:01 +0900

    Feat: 타입 변경, props 변경

--- src/components/common/InterviewBar/index.tsx
+++ src/components/common/InterviewBar/index.tsx
@@ -1,38 +1,41 @@
 'use client';
 import React from 'react';
 
-interface QuestionList {
-  id: number;
-  question: string;
-}
-type current = number;
+type QuestionList = {
+  questionId: number;
+  subcategory: string;
+  questionContent: string;
+  answerContent: string;
+};
 
-export interface InterviewBarProps extends React.HTMLAttributes<HTMLDivElement> {
-  data: QuestionList[];
-  currentId?: current;
-}
+export type InterviewBarProps = {
+  questions: QuestionList[];
+  currentId?: number;
+};
 
-export const InterviewBar = ({ data, currentId = 1 }: InterviewBarProps) => {
-  const widthCur = Number(1100 / (data.length - 1)) + 'px';
+export const InterviewBar = ({ questions, currentId = 0 }: InterviewBarProps) => {
+
+  const widthCur = Number(1100 / (questions.length - 1)) + 'px';
 
   const cur = `width: ${widthCur}px`;
   return (
     <div className="">
       <ul className="flex justify-between w-[1100px] items-center">
-        {data.map(i => (
-          <>
+        {questions.map(question => (
+          <div key={question.questionId}>
             <li
               className={`w-4 h-4 rounded-full border-blue-normal ${
-                i.id <= currentId + 1 ? 'bg-[#606DE3]' : 'bg-white'
+                question.questionId <= currentId + 1 ? 'bg-[#606DE3]' : 'bg-white'
               } border-4`}></li>
-            {data.length !== i.id && (
+            {questions.length !== question.questionId && (
               <li
                 style={{ width: widthCur }}
-                className={` h-[4px] ${i.id <= currentId ? 'bg-blue-normal' : 'bg-[#B1BDDE]'}
-             
+                className={` h-[4px] ${
+                  question.questionId <= currentId ? 'bg-blue-normal' : 'bg-[#B1BDDE]'
+                }
              `}></li>
             )}
-          </>
+          </div>
         ))}
       </ul>
     </div>
