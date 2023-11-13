3476cdb37494193388122044c70808d680cad6c5
dokimion24 <leecy3610@gmail.com>
2023-11-13 16:07:01 +0900

    Feat: Timer 컴포넌트 props 추가

--- src/components/common/Timer/index.tsx
+++ src/components/common/Timer/index.tsx
@@ -1,35 +1,41 @@
 'use client';
 
 import useTimer from '@/hooks/useTimer';
+import { TimerState } from '@/types';
 import React from 'react';
 
 type TimerProps = {
   time?: number;
+  timerState: TimerState;
+  setTimerState: (state: TimerState) => void;
 };
 
-const Timer = ({ time = 60 }: TimerProps) => {
+const Timer = ({ time = 10, timerState, setTimerState }: TimerProps) => {
   const baseStyles = `w-[174px] h-[174px] rounded-full relative flex justify-center items-center`;
 
-  const { remainingTime, isCompleted } = useTimer(time);
+  const { remainingTime } = useTimer(time, timerState, setTimerState);
 
   return (
     <div className="w-[180px] h-[180px] relative">
-      <svg className="circle">
-        <circle
-          className="border"
-          style={{
-            stroke: '#4374F3',
-            strokeWidth: '8',
-            fill: 'transparent',
-            strokeDashoffset: '500',
-            strokeDasharray: '0',
-            animation: `${time}s circletimer linear`,
-          }}
-          cx="90"
-          cy="90"
-          r="80"
-        />
-      </svg>
+      {timerState === 'PROGRESS' && (
+        <svg className="circle">
+          <circle
+            className="border"
+            style={{
+              stroke: '#4374F3',
+              strokeWidth: '8',
+              fill: 'transparent',
+              strokeDashoffset: '500',
+              strokeDasharray: '0',
+              animation: `${time}s circletimer linear`,
+            }}
+            cx="90"
+            cy="90"
+            r="80"
+          />
+        </svg>
+      )}
+
       <span className="timer">{remainingTime}</span>
     </div>
   );
