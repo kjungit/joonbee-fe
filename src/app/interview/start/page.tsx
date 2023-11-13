f499843d717942620c3c6d4fdd26d410ad13d1a5
dokimion24 <leecy3610@gmail.com>
2023-11-13 16:04:29 +0900

    KAN-58 Feat: InterviewStartPage 구현

---
+++ src/app/interview/start/page.tsx
@@ -0,0 +1,11 @@
+import { InterviewStartBox } from '@/components/common/InterviewStartBox';
+
+const InterviewStartPage = () => {
+  return (
+    <div className="h-full flex items-center justify-center bg-main-primary">
+      <InterviewStartBox />
+    </div>
+  );
+};
+
+export default InterviewStartPage;
