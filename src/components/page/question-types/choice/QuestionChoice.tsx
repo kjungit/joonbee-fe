1e86c6322317fbefa70652e28bd4c62f808ae3c7
dokimion24 <leecy3610@gmail.com>
2023-11-17 03:45:28 +0900

    Feat: QuestionChoice 컴포넌트 생성

--- src/components/page/question-types/choice/QuestionChoice.tsx
+++ src/components/page/question-types/choice/QuestionChoice.tsx
@@ -1,18 +1,12 @@
-import QuestionForm from '@/components/common/QuestionForm';
-import React from 'react';
+'use client';
 
-const data = [
-  { category: '프론트엔드', subcategory: ['React', 'Vue', 'Nextjs'] },
-  { category: '백엔드', subcategory: ['DB', 'Express', 'MSA'] },
-  { category: 'CS', subcategory: ['Docker', '운영체제', '컴퓨터구조'] },
-  { category: '모바일', subcategory: ['IOS', '플러터'] },
-  { category: '기타', subcategory: ['Git'] },
-];
+import QuestionForm from '@/components/common/QuestionForm';
+import React, { useState } from 'react';
 
 export default function QuestionChoice() {
   return (
     <section className="w-[1200px] h-[90%] flex flex-col gap-5 bg-background-lightgray px-[50px] py-[40px] rounded-[40px]">
-      <QuestionForm data={data} />
+      <QuestionForm />
     </section>
   );
 }
