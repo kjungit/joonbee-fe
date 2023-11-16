dc66be7c7f2bf933e285efadc6a440c7b61e7ce5
dokimion24 <leecy3610@gmail.com>
2023-11-17 03:56:10 +0900

    Feat: 주석처리

--- src/components/ui/Dropdown/List.tsx
+++ src/components/ui/Dropdown/List.tsx
@@ -1,34 +1,34 @@
-import { selectedValueState } from '../../../recoil/select/atom';
-import { useRecoilState } from 'recoil';
+// import { selectedValueState } from '../../../recoil/select/atom';
+// import { useRecoilState } from 'recoil';
 
-type ListProps = {
-  children: React.ReactNode;
-  name: 'category' | 'subcategory';
-  size: 'sm' | 'md';
-};
+// type ListProps = {
+//   children: React.ReactNode;
+//   name: 'category' | 'subcategory';
+//   size: 'sm' | 'md';
+// };
 
-const List = ({ children, name, size = 'sm' }: ListProps) => {
-  const baseStyles = `bg-main-primary text-white rounded-[8px] font-bold text-center hover:bg-hover-primary shadow-sm`;
-  const sizeStyles = {
-    sm: 'text-[16px] w-[116px] h-[44px]',
-    md: 'text-[20px] w-[160px] h-[60px]',
-  };
+// const List = ({ children, name, size = 'sm' }: ListProps) => {
+//   const baseStyles = `bg-main-primary text-white rounded-[8px] font-bold text-center hover:bg-hover-primary shadow-sm`;
+//   const sizeStyles = {
+//     sm: 'text-[16px] w-[116px] h-[44px]',
+//     md: 'text-[20px] w-[160px] h-[60px]',
+//   };
 
-  const ListStyles = `${baseStyles} ${sizeStyles[size]}`;
+//   const ListStyles = `${baseStyles} ${sizeStyles[size]}`;
 
-  const [selectedValue, setSelectedValue] = useRecoilState(selectedValueState);
+//   const [selectedValue, setSelectedValue] = useRecoilState(selectedValueState);
 
-  const onChangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    name === 'category'
-      ? setSelectedValue(prev => ({ ...prev, selectedCategory: e.target.value }))
-      : setSelectedValue(prev => ({ ...prev, selectedSubcategory: e.target.value }));
-  };
+//   const onChangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+//     name === 'category'
+//       ? setSelectedValue(prev => ({ ...prev, selectedCategory: e.target.value }))
+//       : setSelectedValue(prev => ({ ...prev, selectedSubcategory: e.target.value }));
+//   };
 
-  return (
-    <select name={name} defaultValue="카테고리" onChange={onChangeSelect} className={ListStyles}>
-      {children}
-    </select>
-  );
-};
+//   return (
+//     <select name={name} defaultValue="카테고리" onChange={onChangeSelect} className={ListStyles}>
+//       {children}
+//     </select>
+//   );
+// };
 
-export default List;
+// export default List;
