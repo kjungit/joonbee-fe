5ffdeff5ca4e5da4a8102ca9ae07ef0174fc4234
dokimion24 <leecy3610@gmail.com>
2023-11-17 03:37:09 +0900

    KAN-63 Feat: DropdownCategory 컴포넌트, 스토리 구현

---
+++ src/components/common/DropdownCategory/index.tsx
@@ -0,0 +1,49 @@
+import Dropdown from '@/components/ui/Dropdown';
+import React, { useEffect, useState } from 'react';
+import { questionCategory } from '@/constants/category';
+import { CategoryName, SubcategoryName } from '@/types/question';
+import { useRecoilState } from 'recoil';
+import { selectedCategoryState, selectedSubcategoryState } from '@/recoil/select/atom';
+
+type DropdownCategoryProps = {
+  color?: 'white' | 'darkNavy';
+};
+export default function DropdownCategory({ color = 'darkNavy' }: DropdownCategoryProps) {
+  const [selectedCategory, setSelectedCategory] = useRecoilState(selectedCategoryState);
+  const [selectedSubcategory, setSelectedSubcategory] = useRecoilState(selectedSubcategoryState);
+
+  const [subcategoryName, setSubcateogyName] = useState<SubcategoryName[]>([]);
+  const categoryNames = questionCategory.map(item => item.category);
+
+  useEffect(() => {
+    setSubcateogyName(
+      questionCategory.find(item => item.category === selectedCategory)?.subcategory || [],
+    );
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    setSelectedSubcategory('세부 카테고리');
+  }, [selectedCategory]);
+
+  const handleCategorySelect: (item: CategoryName) => void = item => {
+    setSelectedCategory(item);
+  };
+
+  return (
+    <div className="flex gap-5">
+      <Dropdown
+        data={categoryNames}
+        selected={selectedCategory}
+        onSelect={handleCategorySelect}
+        color={color}
+      />
+      <Dropdown
+        data={subcategoryName}
+        selected={selectedSubcategory}
+        onSelect={setSelectedSubcategory}
+        title="세부 카테고리"
+        color={color}
+      />
+    </div>
+  );
+}
