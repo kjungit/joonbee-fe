cca15a48c4e39a98a702aea318a627bd78d586ab
dokimion24 <leecy3610@gmail.com>
2023-11-18 18:39:27 +0900

    KAN-72 Feat: CategorizedQuestionCard 컴포넌트, 스토리 수정

--- src/components/common/CategorizedQuestionCard/index.tsx
+++ src/components/common/CategorizedQuestionCard/index.tsx
@@ -1,52 +1,66 @@
 'use client';
+import { Icon } from '@/components/ui/Icon';
+import { MyQuestion } from '@/recoil/myQuestion/atom';
+import { myQuestionAddSelector } from '@/recoil/myQuestion/withAdd';
+import { myQuestionClickSelector } from '@/recoil/myQuestion/withClick';
+import { CategoryName, SubcategoryName } from '@/types/question';
 import React from 'react';
+import { useRecoilState } from 'recoil';
 
 type CardSize = 'md' | 'lg';
-type Border = boolean;
-interface Question {
-  title: string;
-  subTitle: string;
-  question: string;
-}
 
-export interface CategorizedQuestionCardProps extends React.HTMLAttributes<HTMLDivElement> {
+interface CategorizedQuestionCardProps extends MyQuestion {
   size?: CardSize;
-  isFocus?: Border;
-  data: Question;
 }
-
 export const CategorizedQuestionCard = ({
   size = 'md',
-  isFocus = false,
-  data,
+  categoryName,
+  subcategoryName,
+  questionContent,
+  questionId,
+  isChecked,
 }: CategorizedQuestionCardProps) => {
-  const baseStyles =
-    'flex h-[54px] text-[18px] px-[10px] justify-between items-center font-bold shadow-md rounded-xl';
+  const [myQuestion, setMyQuestion] = useRecoilState(myQuestionClickSelector);
+
+  const baseStyles = `flex h-[54px] text-[18px] px-[10px] justify-between items-center font-bold shadow-md
+     rounded-[8px] bg-white cursor-pointer`;
 
   const sizeStyles = {
     md: 'w-[900px] text-[18px]',
-    lg: 'w-[1100px] text-[20px]',
+    lg: 'w-[1100px] text-[20px] min-h-[54px]',
   };
 
   const textStyles = 'flex justify-center w-[190px]';
 
-  const borderPositionStyles = 'border-main-primary border-4';
+  const borderPositionStyles = 'border-main-primary border-2';
   const buttonStyles = `
   ${baseStyles}
   ${sizeStyles[size]}
-  ${isFocus ? borderPositionStyles : 'border-white border-4'}
+  ${isChecked ? borderPositionStyles : 'border-white border-2'}
   `;
 
+  const onClickQuestion = () => {
+    setMyQuestion([
+      {
+        categoryName,
+        subcategoryName,
+        questionContent,
+        questionId,
+        isChecked: !isChecked,
+      },
+    ]);
+  };
+
   return (
-    <div className={buttonStyles}>
+    <div className={buttonStyles} onClick={onClickQuestion}>
       <div className="flex">
-        <div className={textStyles}>{data.title}</div>
+        <div className={textStyles}>{categoryName}</div>
         <div className="border-l-gray-normal border-l-2" />
-        <div className={textStyles}>{data.subTitle}</div>
+        <div className={textStyles}>{subcategoryName}</div>
         <div className="border-l-gray-normal border-l-2" />
-        <div className="ml-10">{data.question}</div>
+        <div className="ml-10">{questionContent}</div>
       </div>
-      {isFocus && <div>icon</div>}
+      {isChecked && <Icon name="check" />}
     </div>
   );
 };
