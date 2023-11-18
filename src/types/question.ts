5e40fe7b089eae1701247f0c5a9f0348fa1721ca
dokimion24 <leecy3610@gmail.com>
2023-11-18 18:41:40 +0900

    Feat: All 카테고리 추가

--- src/types/question.ts
+++ src/types/question.ts
@@ -1,4 +1,12 @@
-export type CategoryName = '프론트엔드' | '백엔드' | 'CS' | '모바일' | '기타' | '언어' | '카테고리';
+export type CategoryName =
+  | '프론트엔드'
+  | '백엔드'
+  | 'CS'
+  | '모바일'
+  | '기타'
+  | '언어'
+  | '카테고리'
+  | 'All';
 export type SubcategoryName =
   | '세부 카테고리'
   | 'React'
