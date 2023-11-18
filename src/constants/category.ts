f6b1589b9e064590aab66d2bd03fa271ff7c19f4
dokimion24 <leecy3610@gmail.com>
2023-11-18 18:41:15 +0900

    Feat: All 카테고리 추가

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,6 +1,7 @@
 import { QuestionCategory } from '@/types/question';
 
 export const questionCategory: QuestionCategory[] = [
+  { category: 'All', subcategory: [] },
   { category: '프론트엔드', subcategory: ['React', 'Vue', 'Nextjs'] },
   { category: '백엔드', subcategory: ['DB', 'Express', 'MSA'] },
   { category: 'CS', subcategory: ['Docker', '운영체제', '컴퓨터구조'] },
