068a57b21dc5d6812ef89d254c2786961f07a31e
dokimion24 <leecy3610@gmail.com>
2023-11-20 01:12:25 +0900

    Feat: 선택된 질문 개수 표시

--- src/components/page/question-types/choice/QuestionChoice.tsx
+++ src/components/page/question-types/choice/QuestionChoice.tsx
@@ -4,14 +4,17 @@ import { CategorizedQuestionCard } from '@/components/common/CategorizedQuestion
 import QuestionForm from '@/components/common/QuestionForm';
 import { Button } from '@/components/ui/Button';
 import { myQuestionAtom } from '@/recoil/myQuestion/atom';
+import { myQuestionClickSelector } from '@/recoil/myQuestion/withClick';
+import { myQuestionFilterSelector } from '@/recoil/myQuestion/withFilter';
 import React, { useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
 export default function QuestionChoice() {
-  const questions = useRecoilValue(myQuestionAtom);
-
+  const questions = useRecoilValue(myQuestionFilterSelector);
+  const filteredQuestion = useRecoilValue(myQuestionClickSelector);
   return (
     <section className="w-[1200px] h-[90%] flex flex-col gap-5 bg-background-lightgray px-[50px] py-[40px] rounded-[40px] relative">
+      <h2 className="text-[32px] font-bold">질문을 준비해주세요</h2>
       <QuestionForm />
       <div className="flex flex-col gap-5 scroll-hide overflow-y-scroll pb-2">
         {questions.map(question => (
@@ -28,7 +31,7 @@ export default function QuestionChoice() {
       </div>
       <div className="h-[80px]">
         <Button color="blueSecondary" size="lg" className="absolute bottom-9 right-[50px]">
-          N개 선택된 질문 보기
+          {filteredQuestion.length}개 선택된 질문 보기
         </Button>
       </div>
     </section>
