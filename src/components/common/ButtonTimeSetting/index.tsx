472329fe90f613d661bdb10062457f9251287488
dokimion24 <leecy3610@gmail.com>
2023-11-20 16:12:04 +0900

    Feat: ButtonTimerSetting 컴포넌트 생성

---
+++ src/components/common/ButtonTimeSetting/index.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Button } from '@/components/ui/Button';
+import React, { useState } from 'react';
+
+export default function ButtonTimeSetting() {
+  const [clickedTime, setClickedTime] = useState(60);
+
+  const onClick = (time: number) => {
+    setClickedTime(time);
+  };
+
+  return (
+    <div className="flex gap-5">
+      <Button color="darkNavy" text="sm">
+        질문 시간 설정
+      </Button>
+      <Button
+        color={`${clickedTime === 60 ? 'darkNavy' : 'white'}`}
+        size="setting"
+        text="sm"
+        onClick={() => onClick(60)}>
+        1분
+      </Button>
+      <Button
+        color={`${clickedTime === 90 ? 'darkNavy' : 'white'}`}
+        size="setting"
+        text="sm"
+        onClick={() => onClick(90)}>
+        1분 30초
+      </Button>
+      <Button
+        color={`${clickedTime === 120 ? 'darkNavy' : 'white'}`}
+        size="setting"
+        text="sm"
+        onClick={() => onClick(120)}>
+        2분
+      </Button>
+    </div>
+  );
+}
