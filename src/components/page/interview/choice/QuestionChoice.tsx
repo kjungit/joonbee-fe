ec1a5be8928b4ee2e3cfffee072eb61c6f5ee132
dokimion24 <leecy3610@gmail.com>
2023-11-20 16:12:51 +0900

    Design: 스타일 수정

--- src/components/page/interview/choice/QuestionChoice.tsx
+++ src/components/page/interview/choice/QuestionChoice.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 import { CategorizedQuestionCard } from '@/components/common/CategorizedQuestionCard';
 import QuestionForm from '@/components/common/QuestionForm';
 import { Button } from '@/components/ui/Button';
@@ -16,7 +18,10 @@ export default function QuestionChoice() {
     <section className="w-[1200px] h-[90%] flex flex-col gap-5 bg-background-lightgray px-[50px] py-[40px] rounded-[40px] relative">
       <h2 className="text-[32px] font-bold">질문을 준비해주세요</h2>
       <QuestionForm />
-      <div className="flex flex-col gap-5 scroll-hide overflow-y-scroll pb-2 items-center">
+      <div
+        className={`flex flex-col gap-5 scroll-hide overflow-y-scroll pb-2 items-center h-[70%] ${
+          questions.length === 0 && 'justify-center'
+        }`}>
         {questions.map(question => (
           <CategorizedQuestionCard
             questionId={question.questionId}
@@ -31,11 +36,11 @@ export default function QuestionChoice() {
         {questions.length === 0 && <NoQuestionMessage />}
       </div>
 
-      <div className="h-[80px]">
+      <Link href="/interview/choice/setting">
         <Button color="blueSecondary" size="lg" className="absolute bottom-9 right-[50px]">
           {ClickedQuestion.length}개 선택된 질문 보기
         </Button>
-      </div>
+      </Link>
     </section>
   );
 }
