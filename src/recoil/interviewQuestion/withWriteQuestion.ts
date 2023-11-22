0238ec012bbfe0591411497c42222fc804d0a99f
dokimion24 <leecy3610@gmail.com>
2023-11-22 09:53:59 +0900

    Feat: 불필요한 recoil 제거

--- src/recoil/interviewQuestion/withWriteQuestion.ts
+++ src/recoil/interviewQuestion/withWriteQuestion.ts
@@ -1,36 +1,19 @@
 import { selector } from 'recoil';
-import { Question, interviewQuestionAtom } from './atom';
+import { interviewAtom } from './atom';
 import { CategoryName } from '@/types/question';
-
-export const questionsSelector = selector({
-  key: 'questionsSelector',
-  get: ({ get }) => {
-    const interviewQuestion = get(interviewQuestionAtom);
-    return interviewQuestion.questions;
-  },
-  set: ({ set, get }, newValue) => {
-    const currentQuestions = get(interviewQuestionAtom).questions;
-
-    const updatedQuestion = {
-      ...get(interviewQuestionAtom),
-      questions: [...currentQuestions, ...(newValue as Question[])],
-    };
-    set(interviewQuestionAtom, updatedQuestion);
-  },
-});
 
 export const categoryNameSelector = selector({
   key: 'categoryNameSelector',
   get: ({ get }) => {
-    const interviewQuestion = get(interviewQuestionAtom);
+    const interviewQuestion = get(interviewAtom);
     return interviewQuestion.categoryName;
   },
   set: ({ set, get }, newValue) => {
     const updatedCategoryName = {
-      ...get(interviewQuestionAtom),
+      ...get(interviewAtom),
       categoryName: newValue as CategoryName,
     };
 
-    set(interviewQuestionAtom, updatedCategoryName);
+    set(interviewAtom, updatedCategoryName);
   },
 });
