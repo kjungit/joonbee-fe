249aa96827c6a018b5572244c9e8926d9892a0b9
kjun <kjun_all@naver.com>
2023-11-29 23:54:33 +0900

    Feat: 불필요한 속성 제거

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_KAKAO_REST_API_KEY: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
-  },
   async rewrites() {
     return [
       {
