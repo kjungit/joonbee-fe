5c7d82871bed2321aad7d3e91cbfb6783740b7c4
kjun <kjun_all@naver.com>
2023-11-29 23:26:52 +0900

    Feat: next.config.js 설정 - kakao restkey - rewrites 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_KAKAO_REST_API_KEY: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://back.joonbee.co.kr/api/:path*',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
