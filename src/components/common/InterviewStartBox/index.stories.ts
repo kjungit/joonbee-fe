f0d82a78c327646a55c78ee34699808c626e75af
dokimion24 <leecy3610@gmail.com>
2023-11-29 09:12:05 +0900

    Feat: storybook 오류 수정

--- src/components/common/InterviewStartBox/index.stories.ts
+++ src/components/common/InterviewStartBox/index.stories.ts
@@ -5,21 +5,17 @@ const meta: Meta<typeof InterviewStartBox> = {
   title: 'DESIGN SYSTEM/Atoms/InterviewStartBox',
   component: InterviewStartBox,
   tags: ['autodocs'],
-  argTypes: {
-    color: {
-      description: 'The type of button',
-      control: 'inline-radio',
-      options: ['gray', 'navy'],
+  parameters: {
+    nextjs: {
+      appDirectory: true,
     },
   },
+  argTypes: {},
 };
 
 export default meta;
 type Story = StoryObj<typeof InterviewStartBox>;
 
 export const Primary: Story = {
-  args: {
-    color: 'navy',
-    title: 'AI면접 시작하기',
-  },
+  args: {},
 };
