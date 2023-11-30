3141fce479b027bbeb526e9b86c223c21c2d99cd
dokimion24 <leecy3610@gmail.com>
2023-11-30 16:43:14 +0900

    Feat: close 아이콘 추가

--- src/components/ui/VariableIcon/index.tsx
+++ src/components/ui/VariableIcon/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BiLike, BiSolidLike } from 'react-icons/bi';
 import { HiOutlineClipboardDocumentList } from 'react-icons/hi2';
+import { IoMdClose } from 'react-icons/io';
+
 import { GoBell } from 'react-icons/go';
 
-export type IconName = 'emptyLike' | 'copy' | 'filledLike' | 'alarm';
+export type IconName = 'emptyLike' | 'copy' | 'filledLike' | 'alarm' | 'close';
 
 export type VariableIconProps = {
   name: IconName;
@@ -18,9 +20,10 @@ export const VariableIcon = ({ size = 24, name, color = 'black', onClick }: Vari
     filledLike: BiSolidLike,
     copy: HiOutlineClipboardDocumentList,
     alarm: GoBell,
+    close: IoMdClose,
   };
 
   const Icon = components[name];
 
-  return <Icon size={size} color={color} onClick={onClick} />;
+  return <Icon size={size} color={color} onClick={onClick} className='cursor-pointer' />;
 };
