440a73727032c9b10653314f112d3011940bc141
dokimion24 <leecy3610@gmail.com>
2023-12-16 11:33:32 +0900

    Feat: RandomPage 생성

--- src/app/interview/random/page.tsx
+++ src/app/interview/random/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import QuestionSettingButton from '@/components/page/interview/random/QuestionSettingButton';
+import { Button } from '@/components/ui/Button';
+import Link from 'next/link';
+import React from 'react';
 
 export default function RandomPage() {
   return (
-    <div>
-      
+    <div className="h-full flex justify-center items-center bg-main-primary">
+      <section className="w-[1200px] h-[90%] flex flex-col gap-5 bg-background-lightgray px-[50px] py-[40px] rounded-[40px] overflow-scroll flex flex-col gap-5 bg-background-lightgray px-[50px] py-[40px] rounded-[40px] overflow-scroll relative">
+        <h2 className="text-[20px] font-bold">랜덤 질문을 준비해주세요</h2>
+        <QuestionSettingButton />
+        <Link href="/interview/permisson">
+          <Button color="blueSecondary" size="lg" className="absolute bottom-9 right-[50px]">
+            랜덤 면접 질문 보기
+          </Button>
+        </Link>
+      </section>
     </div>
-  )
+  );
 }
