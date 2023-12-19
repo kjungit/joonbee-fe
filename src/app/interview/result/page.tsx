1eeac8253015656eb13838fac8e6dd6eab00ce40
dokimion24 <leecy3610@gmail.com>
2023-12-19 12:15:26 +0900

    Feat: ResultPage 컴포넌트 작성

---
+++ src/app/interview/result/page.tsx
@@ -0,0 +1,13 @@
+import InterviewResult from '@/components/page/interview/result/InterviewResult';
+import React from 'react';
+
+export default function ResultPage() {
+  return (
+    <div className="h-full flex justify-center items-center bg-main-primary">
+      <section className="w-[1024px] h-[706px] flex flex-col gap-5 bg-background-lightgray p-8 rounded-[40px] relative">
+        <h2 className="text-[20px] font-bold">면접 결과</h2>
+        <InterviewResult />
+      </section>
+    </div>
+  );
+}
