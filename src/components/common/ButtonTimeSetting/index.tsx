2b1211b985e35b25d29bfbb1a9a0e0ffd75d3b02
dokimion24 <leecy3610@gmail.com>
2023-12-19 17:44:41 +0900

    Feat: 사이즈, 스타일 수정

--- src/components/common/ButtonTimeSetting/index.tsx
+++ src/components/common/ButtonTimeSetting/index.tsx
@@ -1,40 +1,36 @@
 'use client';
-
 import { Button } from '@/components/ui/Button';
 import { interviewTimeAtom } from '@/recoil/interviewSetting/atoms';
 import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
-
 export default function ButtonTimeSetting() {
   const [clickedTime, setClickedTime] = useRecoilState(interviewTimeAtom);
-
   const onClick = (time: number) => {
     setClickedTime(time);
   };
-
   return (
     <div className="flex gap-5">
-      <Button color="darkNavy" text="sm">
+      <Button color="darkNavy" text="xs" size="auto">
         질문 시간 설정
       </Button>
       <Button
         color={`${clickedTime === 60 ? 'darkNavy' : 'white'}`}
         size="setting"
-        text="sm"
+        text="xs"
         onClick={() => onClick(60)}>
         1분
       </Button>
       <Button
         color={`${clickedTime === 90 ? 'darkNavy' : 'white'}`}
         size="setting"
-        text="sm"
+        text="xs"
         onClick={() => onClick(90)}>
         1분 30초
       </Button>
       <Button
         color={`${clickedTime === 120 ? 'darkNavy' : 'white'}`}
         size="setting"
-        text="sm"
+        text="xs"
         onClick={() => onClick(120)}>
         2분
       </Button>
