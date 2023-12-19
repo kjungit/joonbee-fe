e7647f852f355213f8afd20793cbc7a9b8efab62
dokimion24 <leecy3610@gmail.com>
2023-12-19 12:12:53 +0900

    Feat: getRandomInterview -> getRandomQuestions, 유저 질문등록 get api 작성

--- src/app/apis/services/cart.ts
+++ src/app/apis/services/cart.ts
@@ -1,18 +1,60 @@
 import { CategoryName, SubcategoryName } from '@/types/question';
 import { instance } from '../axios';
 
-type Params = {
+type RandomQuestionsParams = {
   category: CategoryName;
   subcategory: SubcategoryName[];
   questionCount: number;
 };
 
-export const getRandomInterview = async ({ category, subcategory, questionCount }: Params) => {
+type UserQuestionsCategoryParams = {
+  category: CategoryName;
+  page: number;
+};
+
+type UserQuestionsSubcategoryParams = {
+  subcategory: SubcategoryName;
+  page: number;
+};
+
+export const getRandomQuestions = async ({
+  category,
+  subcategory,
+  questionCount,
+}: RandomQuestionsParams) => {
   const res = await instance().get('/api/question/gpt', {
     params: {
       category,
       subcategory,
       questionCount,
+    },
+  });
+
+  return res.data.data.result;
+};
+
+export const getUserQuestionsCategory = async ({
+  category,
+  page = 1,
+}: UserQuestionsCategoryParams) => {
+  const res = await instance().get('/api/cart/questions', {
+    params: {
+      category,
+      page,
+    },
+  });
+
+  return res.data.data.result;
+};
+
+export const getUserQuestionsSubcategory = async ({
+  subcategory,
+  page = 1,
+}: UserQuestionsSubcategoryParams) => {
+  const res = await instance().get('/api/cart/questions', {
+    params: {
+      subcategory,
+      page,
     },
   });
 
