8351d519e33256e5aff3c3e7556821b1aef1c65b
dokimion24 <leecy3610@gmail.com>
2023-12-22 08:43:22 +0900

    Feat: 사용자 질문 api 추가

--- src/app/apis/services/cart.ts
+++ src/app/apis/services/cart.ts
@@ -1,31 +1,35 @@
-import { CategoryName, SubcategoryName } from '@/types/question';
+import { AllCategory, CategoryName, QuestionCategory, SubcategoryName } from '@/types/question';
 import { instance } from '../axios';
 
-type RandomQuestionsParams = {
-  category: CategoryName;
-  subcategory: SubcategoryName[];
+export type RandomQuestionsParams = QuestionCategory & {
   questionCount: number;
 };
 
-type UserQuestionsCategoryParams = {
-  category: CategoryName;
-  page: number;
+type UserQuestionsParams = AllCategory & {
+  page?: number;
 };
 
-type UserQuestionsSubcategoryParams = {
-  subcategory: SubcategoryName;
-  page: number;
+type QuestionData = AllCategory & {
+  questionContent: string;
 };
 
+export type UserQuestionsResponseData = QuestionData & {
+  questionId: string;
+};
+
+/**
+ * 랜덤 질문들 가져오기 api
+ */
 export const getRandomQuestions = async ({
   category,
   subcategory,
   questionCount,
 }: RandomQuestionsParams) => {
+  const subcategories = [...subcategory];
   const res = await instance().get('/api/question/gpt', {
     params: {
       category,
-      subcategory,
+      subcategory: subcategories,
       questionCount,
     },
   });
@@ -33,29 +37,42 @@ export const getRandomQuestions = async ({
   return res.data.data.result;
 };
 
-export const getUserQuestionsCategory = async ({
+/**
+ * 사용자가 추가한 질문들 가져오기 api
+ */
+export const getUserQuestions = async ({
   category,
+  subcategory,
   page = 1,
-}: UserQuestionsCategoryParams) => {
+}: UserQuestionsParams) => {
+  category = category === 'All' ? '' : category;
+
   const res = await instance().get('/api/cart/questions', {
     params: {
       category,
+      subcategory,
       page,
     },
   });
 
+  console.log('res', res.data.data.result);
+
   return res.data.data.result;
 };
 
-export const getUserQuestionsSubcategory = async ({
+/**
+ * 사용자 질문 등록 api
+ */
+export const postUserQuestion = async ({
+  category,
   subcategory,
-  page = 1,
-}: UserQuestionsSubcategoryParams) => {
-  const res = await instance().get('/api/cart/questions', {
-    params: {
-      subcategory,
-      page,
-    },
+  questionContent,
+}: QuestionData) => {
+  console.log('api 호출');
+  const res = await instance().post('/api/cart/question/save', {
+    categoryName: category,
+    subcategoryName: subcategory,
+    questionContent,
   });
 
   return res.data.data.result;
