e320454cf394da8a8470297c290b410726999791
dokimion24 <leecy3610@gmail.com>
2023-12-22 14:10:02 +0900

    Feat: isChecked -> isClicked, className props 추가, 디자인 수정

--- src/components/common/CategorizedQuestionCard/index.tsx
+++ src/components/common/CategorizedQuestionCard/index.tsx
@@ -1,10 +1,7 @@
 'use client';
 import { UserQuestionsResponseData } from '@/app/apis/services/cart';
 import { Icon } from '@/components/ui/Icon';
-import { MyQuestion } from '@/recoil/myQuestion/atom';
-import { myQuestionAddSelector } from '@/recoil/myQuestion/withAdd';
 import { myQuestionClickSelector } from '@/recoil/myQuestion/withClick';
-import { CategoryName, SubcategoryName } from '@/types/question';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 import { useRecoilState } from 'recoil';
@@ -13,7 +10,8 @@ type CardSize = 'sm' | 'md' | 'lg';
 
 type CategorizedQuestionCardProps = UserQuestionsResponseData & {
   size?: CardSize;
-  isChecked?: boolean;
+  isClicked?: boolean;
+  className?: string;
 };
 export const CategorizedQuestionCard = ({
   size = 'md',
@@ -21,47 +19,49 @@ export const CategorizedQuestionCard = ({
   subcategory,
   questionContent,
   questionId,
-  isChecked = false,
+  isClicked = false,
+  className,
 }: CategorizedQuestionCardProps) => {
   const pathname = usePathname();
 
   const [myQuestion, setMyQuestion] = useRecoilState(myQuestionClickSelector);
 
-  const baseStyles = `flex px-[10px] justify-between items-center font-bold shadow-md
-     rounded-[8px] bg-white cursor-pointer`;
+  const baseStyles = `flex px-[10px] justify-between items-center font-bold shadow-md shrink-0
+     rounded-[8px] bg-white cursor-pointer ${className}`;
 
   const sizeStyles = {
-    sm: 'w-full text-[14px] h-[42px]',
-    md: 'w-[900px] text-[18px] h-[54px]',
+    sm: 'w-full text-[14px] h-[46px]',
+    md: 'w-full text-[18px] h-[54px]',
     lg: 'w-[1100px] text-[20px] min-h-[54px] h-[54px]',
   };
 
   const textStyles = 'flex justify-center w-[190px]';
 
   const borderPositionStyles = 'border-main-primary border-2';
-  const buttonStyles = `
+  const cardStyles = `
   ${baseStyles}
   ${sizeStyles[size]}
-  ${isChecked ? borderPositionStyles : 'border-white border-2'}
+  ${isClicked ? borderPositionStyles : 'border-white border-2'}
   `;
 
   const onClickQuestion = () => {
-    if (pathname === '/interview/choice/setting') {
-      return;
+    if (pathname === '/interview/choice') {
+      setMyQuestion([
+        {
+          category,
+          subcategory,
+          questionContent,
+          questionId,
+          isClicked: !isClicked,
+        },
+      ]);
     }
-    setMyQuestion([
-      {
-        category,
-        subcategory,
-        questionContent,
-        questionId,
-        isChecked: !isChecked,
-      },
-    ]);
   };
 
+  console.log(isClicked);
+
   return (
-    <div className={buttonStyles} onClick={onClickQuestion}>
+    <div className={cardStyles} onClick={onClickQuestion}>
       <div className="flex">
         <div className={textStyles}>{category}</div>
         <div className="border-l-gray-normal border-l-2" />
@@ -69,7 +69,7 @@ export const CategorizedQuestionCard = ({
         <div className="border-l-gray-normal border-l-2" />
         <div className="ml-10">{questionContent}</div>
       </div>
-      {isChecked && <Icon name="check" />}
+      {isClicked && <Icon name="check" />}
     </div>
   );
 };
