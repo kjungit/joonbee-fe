bf523a66cd72ff90f6d43dc56555714a61b81d3d
kjun <kjun_all@naver.com>
2023-12-26 22:44:52 +0900

    Feat: PolarChart 컴포넌트 수정

--- src/components/common/PolarChart/index.tsx
+++ src/components/common/PolarChart/index.tsx
@@ -1,11 +1,21 @@
 'use client';
+import { CategoryInfoProps } from '@/hooks/useUserInfo';
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import ReactApexChart from 'react-apexcharts';
 type PolarChartProps = {
-  data: number[];
+  data: CategoryInfoProps[];
 };
 export const PolarChart = ({ data }: PolarChartProps) => {
   const [currentSeries, setCurrentSeries] = useState([21, 17, 15, 10, 12, 17, 21, 22]);
+  const [currentLabels, setCurrentLabels] = useState([
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'Vue',
+    '네트워크',
+    'cs',
+    '프로젝트',
+  ]);
   const chartData = {
     series: currentSeries,
     options: {
@@ -15,15 +25,18 @@ export const PolarChart = ({ data }: PolarChartProps) => {
       fill: {
         opacity: 0.8,
       },
-      labels: ['JavaScript', 'TypeScript', 'React', 'Vue', '네트워크', 'cs', '프로젝트'],
+      labels: currentLabels,
     },
   };
   useEffect(() => {
-    setCurrentSeries(data);
+    const count_list = data.map(item => item.categoryCount);
+    const name_list = data.map(item => item.categoryName);
+    setCurrentSeries(count_list);
+    setCurrentLabels(name_list);
   }, [data]);
 
   return (
-    <div id="chart" className="w-[330px]">
+    <div id="chart" className="w-[280px]">
       <ReactApexChart options={chartData.options} series={chartData.series} type="polarArea" />
     </div>
   );
