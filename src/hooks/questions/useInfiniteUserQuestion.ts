67e99793df80af90a997c8b103651190752c276a
dokimion24 <leecy3610@gmail.com>
2023-12-27 03:03:27 +0900

    Feat: All -> 세부 카테고리, flatMap -> concat

--- src/hooks/questions/useInfiniteUserQuestion.ts
+++ src/hooks/questions/useInfiniteUserQuestion.ts
@@ -11,29 +11,25 @@ export default function useInfiniteUserQuestion(
   subcategory: SubcategoryName,
 ) {
   const getKey = (page: number, previousPageData: UserQuestionsResponseData[]) => {
+
     const newPage = page + 1;
     if (previousPageData && !previousPageData.length) {
       return null;
     }
-    const newCategory = category === 'All' ? '' : category;
-    return `/api/cart/questions?page=${newPage}&category=${newCategory}&subcategory=${subcategory}`;
+    const newSubcategory = subcategory === '세부 카테고리' ? '' : subcategory;
+    return `/api/cart/questions?page=${newPage}&category=${category}&subcategory=${newSubcategory}`;
   };
 
   const { data, error, size, setSize, isValidating } = useSWRInfinite<UserQuestionsResponseData[]>(
     getKey,
     url => getUserQuestions(url),
     {
-      initialSize: 1,
       revalidateFirstPage: false,
     },
   );
 
-  const newData = data?.flatMap(data => data);
-  console.log('new', newData);
-
-  const isFetching = !isValidating;
+  const newData = data ? ([] as UserQuestionsResponseData[]).concat(...data) : [];
 
   const { setTarget } = useIntersectionObserver(setSize);
-
   return { newData, error, size, isValidating, setTarget, setSize };
 }
