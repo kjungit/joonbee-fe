963b27b0ad3c386d14d5c1c7aa82f16d6f7d023d
dokimion24 <leecy3610@gmail.com>
2023-12-26 20:39:15 +0900

    Feat: 인자 타입 추가

--- src/hooks/questions/useMutateUserQuestion.ts
+++ src/hooks/questions/useMutateUserQuestion.ts
@@ -1,10 +1,14 @@
-import { ResponseData, postUserQuestion } from '@/app/apis/services/cart';
-import { selectedCategoryAtom, selectedSubcategoryAtom } from '@/recoil/selectedCategory/atom';
+import { postUserQuestion } from '@/app/apis/services/cart';
+import { CategoryName, SubcategoryName } from '@/types/question';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import useSWRMutation from 'swr/mutation';
 
-export default function useMutateUserQuestion(category, subcategory, questionContent) {
+export default function useMutateUserQuestion(
+  category: CategoryName,
+  subcategory: SubcategoryName,
+  questionContent: string,
+) {
   const { trigger, data } = useSWRMutation('/api/cart/question/save', () =>
     postUserQuestion({ category, subcategory, questionContent }),
   );
