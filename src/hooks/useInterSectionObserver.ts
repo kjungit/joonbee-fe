903163c7685c595f32df0d9a1df00e6d2f2f9f2e
dokimion24 <leecy3610@gmail.com>
2023-12-26 20:38:10 +0900

    Feat: 데이터 중복 요청 수정

--- src/hooks/useInterSectionObserver.ts
+++ src/hooks/useInterSectionObserver.ts
@@ -1,16 +1,23 @@
 import { UserQuestionsResponseData } from '@/app/apis/services/cart';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-export const useIntersectionObserver = (setSize: any, isLoading: boolean) => {
+export const useIntersectionObserver = (setSize: any, isFetching: boolean) => {
   const [target, setTarget] = useState<HTMLDivElement | null>(null);
+  const [shouldLoadMore, setShouldLoadMore] = useState(false);
 
-  const observerCallback: IntersectionObserverCallback = entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting && isLoading) {
-        setSize((prev: any) => prev + 1);
-      }
-    });
-  };
+  useEffect(() => {
+    if (shouldLoadMore) setSize((prev: any) => prev + 1);
+  }, [shouldLoadMore]);
+
+  const observerCallback = useMemo(() => {
+    return (entries: IntersectionObserverEntry[]) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setShouldLoadMore(true);
+        }
+      });
+    };
+  }, []);
 
   useEffect(() => {
     if (!target) return;
