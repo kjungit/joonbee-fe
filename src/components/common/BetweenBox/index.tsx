cbf92973f5fdca182f4123ef575dfb035b4db2de
kjun <kjun_all@naver.com>
2023-12-26 22:40:57 +0900

    Feat: BetweenBox 컴포넌트 스타일 수정

--- src/components/common/BetweenBox/index.tsx
+++ src/components/common/BetweenBox/index.tsx
@@ -9,7 +9,7 @@ interface BetweenBoxProps {
 export const BetweenBox = ({ first, second }: BetweenBoxProps) => {
   return (
     <div
-      className={`w-[254px] rounded-md h-[50px] bg-gray-light flex justify-between px-6 py-3 font-bold text-lg text-main-primary items-center`}>
+      className={`w-full max-w-[240px] rounded-md h-[50px] bg-gray-light flex justify-between px-6 py-3 font-bold text-lg text-main-primary items-center`}>
       <p>{first}</p>
       <p>{second}</p>
     </div>
