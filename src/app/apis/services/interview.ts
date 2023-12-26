58d655d32becd6b3576713334a5d9489cd276770
kjun <kjun_all@naver.com>
2023-12-26 22:39:07 +0900

    Feat: getInterview api 구현

--- src/app/apis/services/interview.ts
+++ src/app/apis/services/interview.ts
@@ -3,14 +3,17 @@ import { instance } from '../axios';
 export const getInterview = async ({
   page = 0,
   category = '',
+  sort = 'latest',
 }: {
   page: number;
   category: string;
+  sort: 'latest' | 'like';
 }) => {
   const res = await instance().get('/api/interview/all', {
     params: {
       page,
       category,
+      sort,
     },
   });
   return res.data.data.result;
