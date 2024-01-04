ba10973dc4fae2be76bf5d366b8c49eda44daf7f
dokimion24 <leecy3610@gmail.com>
2024-01-04 17:26:43 +0900

    Feat: 레이아웃 수정, 제목 추가

--- src/app/interview/choice/setting/page.tsx
+++ src/app/interview/choice/setting/page.tsx
@@ -4,7 +4,10 @@ import React from 'react';
 export default function SettingPage() {
   return (
     <div className="h-full flex justify-center items-center bg-main-primary">
-      <QuestionChocieSetting />
+      <section className="w-[1024px] h-[600px] flex flex-col gap-5 bg-background-lightgray p-8  rounded-[40px] relative">
+        <h2 className="text-[20px] font-bold">면접 전 설정해주세요</h2>
+        <QuestionChocieSetting />
+      </section>
     </div>
   );
 }
