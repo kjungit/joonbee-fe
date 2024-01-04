5d6ab9d5a6af76baa2a594f86dc0c852317e19ea
dokimion24 <leecy3610@gmail.com>
2024-01-04 17:31:21 +0900

    Feat: 질문 데이터 구조 변경

--- src/components/page/interview/progress/ChocieInterview.tsx
+++ src/components/page/interview/progress/ChocieInterview.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Questions from './Questions';
+import { useRecoilValue } from 'recoil';
+import { myQuestionAtom } from '@/recoil/myQuestion/atom';
 
 export default function ChocieInterview() {
+  const questions = useRecoilValue(myQuestionAtom);
+
+  const transformedQuestion = questions.map(question => ({
+    questionId: question.questionId,
+    questionContent: question.questionContent,
+    answerContent: '',
+  }));
+
+  console.log('Choice', transformedQuestion);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Questions questions={transformedQuestion} />
+    </>
+  );
 }
