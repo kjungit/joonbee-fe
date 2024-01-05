8d9f862cf62951549f5ee688601d7251e459ca6d
dokimion24 <leecy3610@gmail.com>
2024-01-05 17:21:20 +0900

    Feat: InterviewRandomContainer 생성

---
+++ src/components/page/interview/random/InterviewRandomContainer.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import QuestionSettingButton from './QuestionSettingButton';
+import { useRouter } from 'next/navigation';
+import InterviewLoading from '@/components/ui/InterviewLoading';
+
+export default function InterviewRandomContainer() {
+  const [isPressedBtn, setIsPressedBtn] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isPressedBtn) {
+      setTimeout(() => {
+        router.push('/interview/permission');
+      }, 3000);
+    }
+  }, [isPressedBtn]);
+
+  const onClick = () => {
+    setIsPressedBtn(true);
+  };
+
+  return (
+    <>
+      {!isPressedBtn ? (
+        <section className="w-[1024px] h-[600px] flex flex-col gap-5 bg-background-lightgray p-8 rounded-[40px] relative">
+          <h2 className="text-[20px] font-bold">랜덤 질문을 준비해주세요</h2>
+          <QuestionSettingButton onClick={onClick} />
+        </section>
+      ) : (
+        <div className="flex flex-col items-center gap-8">
+          <InterviewLoading interviewType="random" />
+          <h2 className="text-white font-bold text-[20px]">랜덤 면접 질문을 준비중입니다!</h2>
+        </div>
+      )}
+    </>
+  );
+}
