1b3a60faa25a6c047a6dfbbedb5327a911f43548
dokimion24 <leecy3610@gmail.com>
2024-01-05 17:22:55 +0900

    Feat: 카테고리 관련 atom 추가

--- src/recoil/selectedCategory/atom.ts
+++ src/recoil/selectedCategory/atom.ts
@@ -6,12 +6,32 @@ export const selectedCategoryAtom = atom<CategoryName>({
   default: '',
 });
 
+export const selectedSubcategoryAtom = atom<SubcategoryName>({
+  key: 'selectedSubcategoryAtom',
+  default: '세부 카테고리',
+});
+
 export const selectedChocieCategoryAtom = atom<CategoryName>({
-  key: 'electedChocieCategoryAtom',
+  key: 'selectedChocieCategoryAtom',
   default: '',
 });
 
-export const selectedSubcategoryAtom = atom<SubcategoryName>({
-  key: 'selectedSubcategoryAtom',
+export const selectedRandomCategoryAtom = atom<CategoryName>({
+  key: 'selectedRandomCategoryAtom',
+  default: '',
+});
+
+export const selectedRandomSubcategoryAtom = atom<SubcategoryName>({
+  key: 'selectedRandomSubcategoryAtom',
+  default: '세부 카테고리',
+});
+
+export const selectedSubmitCategoryAtom = atom<CategoryName>({
+  key: 'selectedSubmitCategoryAtom',
+  default: '',
+});
+
+export const selectedSubmitSubcategoryAtom = atom<SubcategoryName>({
+  key: 'selectedSubmitSubcategoryAtom',
   default: '세부 카테고리',
 });
