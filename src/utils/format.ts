514926b702b151bfb1bfc949d8fb121d29765604
kjun <kjun_all@naver.com>
2024-01-06 23:44:54 +0900

    Feat: maskNickname 로직 수정

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -4,12 +4,11 @@ export const capitalizeFirstLetter = (text: IconName) => {
   return text.charAt(0).toUpperCase() + text.slice(1);
 };
 
-export const maskNickname = (nickname: any) => {
+export const maskNickname = (nickname: string) => {
   if (nickname.length <= 3) {
     return nickname;
   }
-
   const visibleText = nickname.substring(0, 3);
-  const maskedText = '*****';
-  return visibleText + maskedText;
+  const maskedText = new Array(nickname.length - 3).fill('*');
+  return visibleText + maskedText.join('');
 };
