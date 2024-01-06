a7958c87cf719f06fe6d4bbfd12e2ae83dacc23a
kjun <kjun_all@naver.com>
2024-01-06 23:19:39 +0900

    Feat: interview api 수정

--- src/app/apis/services/interview.ts
+++ src/app/apis/services/interview.ts
@@ -1,20 +1,16 @@
 import { instance } from '../axios';
 
-export const getInterview = async ({
-  page = 0,
-  category = '',
-  sort = 'latest',
-}: {
-  page: number;
-  category: string;
-  sort: 'latest' | 'like';
-}) => {
-  const res = await instance().get('/api/interview/all', {
+export const getInterview = async (url: string) => {
+  const res = await instance(false).get(url);
+  return res.data.data.result;
+};
+
+export const deleteInterview = async (id: number) => {
+  const res = await instance().delete('/api/member/interview/delete', {
     params: {
-      page,
-      category,
-      sort,
+      id,
     },
   });
-  return res.data.data.result;
+
+  return res.data;
 };
