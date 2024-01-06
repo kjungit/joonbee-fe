c9e5efc67fa8065803ec23b559f9b2ecfb702585
kjun <kjun_all@naver.com>
2024-01-06 23:21:04 +0900

    Feat: 카카오 로그인 후 로직 추가

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -6,15 +6,22 @@ import { useRecoilState } from 'recoil';
 import { isTokenedState } from '@/recoil/isTokened/atoms';
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import Image from 'next/image';
+import { isLoginedStatus } from '@/recoil/isLogined/atom';
+import { useUserInfo } from '@/hooks/useUserInfo';
 
 export default function OauthPage() {
   const searchParams = useSearchParams();
   const AUTHORIZATION_CODE: string = searchParams.get('code') as string;
   const router = useRouter();
   const [isTokened, setIsTokened] = useRecoilState(isTokenedState);
+  const [isLogined, setisLogined] = useRecoilState(isLoginedStatus);
+  const { userInfoMutate } = useUserInfo();
+
   const { data, error } = useSWR('/auth/kakao', () => kakaoLogin(AUTHORIZATION_CODE), {
     onSuccess: () => {
       router.push('/');
+      setisLogined(true);
+      userInfoMutate();
     },
   });
   useEffect(() => {
