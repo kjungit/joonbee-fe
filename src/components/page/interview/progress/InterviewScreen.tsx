41c847db5ea4e003b68f92e181e5399bb22e6785
dokimion24 <leecy3610@gmail.com>
2024-01-06 23:03:10 +0900

    Feat: 새로고침 시 페이지 이동 기능 추가

--- src/components/page/interview/progress/InterviewScreen.tsx
+++ src/components/page/interview/progress/InterviewScreen.tsx
@@ -4,9 +4,12 @@ import RandomInterview from './RandomInterview';
 import ChocieInterview from './ChocieInterview';
 import { useRecoilValue } from 'recoil';
 import { interviewTypeAtom } from '@/recoil/interviewType/atom';
+import useBeforeUnload from '@/hooks/useBeforeUnload';
 
 export default function InterviewScreen() {
   const type = useRecoilValue(interviewTypeAtom);
+
+  useBeforeUnload();
 
   return (
     <>
