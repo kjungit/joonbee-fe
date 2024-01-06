6af81c359c53567f8e8d925fa9eec60787878b97
kjun <kjun_all@naver.com>
2024-01-06 23:34:21 +0900

    Feat: data 연결 및 like api 연결

--- src/components/page/Main/DetailInterview.tsx
+++ src/components/page/Main/DetailInterview.tsx
@@ -2,44 +2,81 @@ import { DetailQuestionCard } from '@/components/common/DetailQuestionCard';
 import { Avatar } from '@/components/ui/Avartar';
 import { Button } from '@/components/ui/Button';
 import { VariableIcon } from '@/components/ui/VariableIcon';
-import React, { MouseEvent } from 'react';
+import React, { MouseEvent, useEffect } from 'react';
 import { InterviewItemType } from './InterviewSection';
+import { maskNickname } from '@/utils/format';
+import useSWRMutation from 'swr/mutation';
+import { postInterviewLike } from '@/app/apis/services/member';
+import { useSWRConfig } from 'swr';
+import { CategoryName } from '@/types/question';
+import { Category } from '@/constants/category';
 
 export default function DetailInterview({
   item,
   onClickClose,
+  categorySelect,
+  current,
+  infMutate,
 }: {
+  current?: number;
   item: InterviewItemType;
   onClickClose: () => void;
+  categorySelect?: CategoryName;
+  infMutate?: any;
 }) {
+  const { interviewId, categoryName, questions, likeCount, thumbnail, memberId, nickname } = item;
   const onClickOpen = (e: MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
   };
+  const { mutate } = useSWRConfig();
+
+  const { trigger } = useSWRMutation('api/member/like', id => postInterviewLike(interviewId), {
+    onSuccess: () => {
+      mutate(['/api/interview/all', categorySelect, current]);
+      infMutate();
+    },
+  });
+
+  const onClickLike = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    trigger();
+  };
+
+  useEffect(() => {
+    console.log(likeCount);
+  }, [likeCount]);
+
   return (
     <div className="fixed z-40 -translate-x-1/2 p-7 -translate-y-1/2 left-1/2 top-1/2 w-screen h-screen  bg-black/60 shadow-md flex items-center justify-center">
       <div
         onClick={onClickOpen}
         className="bg-white border-4  border-main-primary gap-4 flex flex-col max-w-[800px] p-6 w-full  rounded-2xl">
         <div className="flex justify-between items-center ">
-          <h3 className="font-bold text-xl">프론트엔드</h3>
+          <h3 className="font-bold text-2xl pl-2">{Category[categoryName]}</h3>
           <Button size="xs" color="darkGray" text="sm" onClick={onClickClose}>
             닫기
           </Button>
         </div>
         <div className="flex justify-between items-center border-gray-normal px-2  py-[12px]">
-          <div className="flex gap-2">
-            <Avatar size="md" thumbnail={''} onClick={() => {}} />
-            <p className="font-bold text-[16px]">by kjun</p>
+          <div className="flex gap-2 items-center">
+            <Avatar size="lg" thumbnail={thumbnail} onClick={() => {}} />
+            <p className="font-bold text-[16px]">by {maskNickname(nickname)}</p>
           </div>
-          <div className="flex gap-2">
-            <VariableIcon name="emptyLike" size={20} onClick={() => {}} />
-            <p className="font-bold text-[16px]">{10}</p>
+          <div className="flex gap-2 items-center">
+            <button onClick={onClickLike} className="p-1 ">
+              <VariableIcon name="emptyLike" />
+            </button>
+            <p className="font-bold text-[16px] w-4">{likeCount}</p>
           </div>
         </div>
         <ul className=" gap-4 flex flex-col max-h-[220px] overflow-y-auto p-2">
-          {/* {item.questions.map((question, index) => (
-            <DetailQuestionCard data={{ ...question, index }} key={question.questionId} />
-          ))} */}
+          {questions.map((question, index) => (
+            <DetailQuestionCard
+              question={question.questionContent}
+              questionCount={index + 1}
+              key={question.questionId}
+            />
+          ))}
         </ul>
         <div className="flex justify-end">
           <Button size="lg" text="sm">
