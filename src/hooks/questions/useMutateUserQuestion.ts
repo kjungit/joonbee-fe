b0e1af05ff14c5d44510a622e56b4680e6c9359a
kjun <kjun_all@naver.com>
2024-01-08 23:09:41 +0900

    Feat: onSuccess 후 mutate 및 porps 추가

--- src/hooks/questions/useMutateUserQuestion.ts
+++ src/hooks/questions/useMutateUserQuestion.ts
@@ -1,17 +1,25 @@
 import { postUserQuestion } from '@/app/apis/services/question';
 import { CategoryName, SubcategoryName } from '@/types/question';
-import React from 'react';
-import { useRecoilValue } from 'recoil';
 import useSWRMutation from 'swr/mutation';
+import useInfiniteUserQuestion from './useInfiniteUserQuestion';
 
 export default function useMutateUserQuestion(
   category: CategoryName,
   subcategory: SubcategoryName,
   questionContent: string,
+  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>,
 ) {
+  const { mutate } = useInfiniteUserQuestion(category, subcategory);
+
   const { trigger, data } = useSWRMutation(
     '/api/cart/question/save',
     () => postUserQuestion(category, subcategory, questionContent),
+    {
+      onSuccess: () => {
+        mutate();
+        if (setIsOpen) setIsOpen(true);
+      },
+    },
   );
 
   return { trigger, data };
