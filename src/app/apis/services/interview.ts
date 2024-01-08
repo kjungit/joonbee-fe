7db6bcfdb5ba93749367808582288cf757ce2696
kjun <kjun_all@naver.com>
2024-01-08 22:37:19 +0900

    Feat: add interview api

--- src/app/apis/services/interview.ts
+++ src/app/apis/services/interview.ts
@@ -4,13 +4,3 @@ export const getInterview = async (url: string) => {
   const res = await instance(false).get(url);
   return res.data.data.result;
 };
-
-export const deleteInterview = async (id: number) => {
-  const res = await instance().delete('/api/member/interview/delete', {
-    params: {
-      id,
-    },
-  });
-
-  return res.data;
-};
