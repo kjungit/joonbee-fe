8410a32765bb46d5211412cdf2cc58ac80a3a3eb
dokimion24 <leecy3610@gmail.com>
2024-01-08 18:38:57 +0900

    Feat: onConfirm props 추가

--- src/components/ui/Modal/AlertModal/index.tsx
+++ src/components/ui/Modal/AlertModal/index.tsx
@@ -6,13 +6,15 @@ type AlertModalProps = {
   onClose: () => void;
   title: string;
   body: string;
+  onConfirm?: () => void;
 };
 
-export default function AlertModal({ isOpened, onClose, title, body }: AlertModalProps) {
+export default function AlertModal({ isOpened, onClose, title, body, onConfirm }: AlertModalProps) {
   return (
     <Modal isOpened={isOpened} onClose={onClose}>
       <Modal.Title>{title}</Modal.Title>
       <Modal.Body>{body}</Modal.Body>
+      {onConfirm && <Modal.ConfirmButton onClick={onConfirm}>확인</Modal.ConfirmButton>}
     </Modal>
   );
 }
