b32b03e25af5e012858c5a956927994850fdfabe
kjun <kjun_all@naver.com>
2024-01-18 23:19:08 +0900

    Feat: 스타일 코드 수정

--- src/components/common/DetailQuestionCard/index.tsx
+++ src/components/common/DetailQuestionCard/index.tsx
@@ -13,14 +13,18 @@ export const DetailQuestionCard = ({
   onClick,
 }: DetailQuestionCardProps) => {
   const baseStyles =
-    'flex h-[48px] text-[18px] text-main-primary px-[10px] items-center font-bold shadow-md rounded-xl cursor-pointer';
-  const textStyles = 'flex justify-center ';
+    'xs:justify-normal justify-center flex min-h-[48px] text-[18px] bg-white text-main-primary px-[10px] items-center font-bold shadow-md rounded-xl cursor-pointer border-4 border-white  hover:border-4 hover:border-main-primary';
 
   return (
     <div className={baseStyles} onClick={onClick}>
-      <div className={`${textStyles} text-[16px] w-[190px]`}>질문 {questionCount}</div>
-      <div className="border-l-gray-normal border-l-2 h-[48px]" />
-      <div className={`${textStyles} text-[16px] ml-12`}>{question}</div>
+      <div className={`flex justify-center text-[16px] min-w-[100px] w-full max-w-[150px]`}>
+        질문 {questionCount}
+      </div>
+      <div className="border-l-gray-normal border-l-2 h-3/4 hidden xs:flex" />
+      <div
+        className={`text-[16px] ml-12 hidden xs:block  break-words whitespace-nowrap overflow-hidden text-ellipsis`}>
+        {question}
+      </div>
     </div>
   );
 };
