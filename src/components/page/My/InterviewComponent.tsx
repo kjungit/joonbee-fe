936e32c070a86b3bd61a45d86a01c4a6403ff884
kjun <kjun_all@naver.com>
2024-01-18 23:43:23 +0900

    feat: interview 자세히보기 연결중

--- src/components/page/My/InterviewComponent.tsx
+++ src/components/page/My/InterviewComponent.tsx
@@ -1,14 +1,16 @@
 'use client';
 import { MyInterviewCard } from '@/components/common/MyInterviewCard';
 import { ItemProps, RadioButtonGroup } from '@/components/common/RadioButtonGroup';
+import { Button } from '@/components/ui/Button';
 import ModalPortal from '@/components/ui/ModalPortal';
 import useInfiniteMyInterview from '@/hooks/my/useInfiniteMyInterview';
+import Image from 'next/image';
 import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useCallback, useEffect, useState } from 'react';
 
 export default function InterviewComponent() {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [selectInterview, setSelectInterview] = useState();
   const searchParams = useSearchParams();
   const router = useRouter();
   const [categorySort, setCategorySort] = useState<'my_interview' | 'liked' | null>('my_interview');
@@ -65,15 +67,56 @@ export default function InterviewComponent() {
       </ul>
       {isOpen && (
         <ModalPortal>
-          {
-            <div onClick={onClickOpen}>
-              <div className="fixed z-40 -translate-x-1/2 p-7 -translate-y-1/2 left-1/2 top-1/2 w-screen h-screen  bg-black/60 shadow-md flex items-center justify-center">
-                <div
-                  onClick={onClickOpen}
-                  className="bg-white border-4  border-main-primary gap-4 flex flex-col max-w-[800px] p-6 w-full  rounded-2xl"></div>
+          <div onClick={onClickOpen}>
+            <div
+              onClick={onClickOpen}
+              className="fixed z-40 -translate-x-1/2 p-7 -translate-y-1/2 left-1/2 top-1/2 w-screen h-screen  bg-black/60 shadow-md flex items-center justify-center">
+              <div
+                onClick={e => {
+                  e.stopPropagation();
+                }}
+                className="bg-white border-4  border-main-primary gap-8 flex flex-col max-w-[800px] p-6 w-full  rounded-2xl">
+                <div className="flex justify-between items-center gap-4">
+                  <div className="w-full flex items-center font-bold border-gray-primary border-b-2 pb-2 gap-4">
+                    <h2 className="text-3xl w-24">질문 </h2>
+                    {/* <p>{selectQuestion.questionContent}</p> */}
+                  </div>
+                  <Button size="xs" color="darkGray" text="sm" onClick={() => setIsOpen(false)}>
+                    닫기
+                  </Button>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <div className="flex gap-2">
+                    <Image width={30} height={30} src="/icons/emoji/pencil.png" alt="pencil" />
+                    <h4 className="font-bold text-lg">내가 한 답변이에요.</h4>
+                  </div>
+                  <div className="text-sm break-keep overflow-auto max-h-[140px] border-l-main-primary border-l-[12px] h-[140px] w-full  shadow-md rounded-xl flex flex-col justify-between px-5 py-4">
+                    {/* {selectQuestion.answerContent} */}
+                  </div>
+                </div>
+                <div className="flex gap-4">
+                  <div className="flex flex-col gap-2 w-1/2">
+                    <div className="flex gap-2">
+                      <Image width={30} height={30} src="/icons/emoji/zoom.png" alt="zoom" />
+                      <h4 className="font-bold text-lg">참고하면 좋을것 같아요.</h4>
+                    </div>
+                    <div className="text-sm break-keep overflow-auto border-l-main-primary border-l-[12px] h-[140px] w-full  shadow-md rounded-xl flex flex-col justify-between px-5 py-4">
+                      {/* {selectQuestion.commentary} */}
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2 w-1/2">
+                    <div className="flex gap-2">
+                      <Image width={30} height={30} src="/icons/emoji/bubble.png" alt="bubble" />
+                      <h4 className="font-bold text-lg">답변에 대한 느낌이에요.</h4>
+                    </div>
+                    <div className="text-sm break-keep overflow-auto border-l-main-primary border-l-[12px] h-[140px] w-full  shadow-md rounded-xl flex flex-col justify-between px-5 py-4">
+                      {/* {selectQuestion.evaluation} */}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          }
+          </div>
         </ModalPortal>
       )}
     </>
