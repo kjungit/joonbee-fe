57a99c1877be5fe78c525a7054c540fdec8b8e0a
kjun <kjun_all@naver.com>
2024-01-18 23:12:15 +0900

    Feat: interview/save api 추가

--- src/app/apis/services/interview.ts
+++ src/app/apis/services/interview.ts
@@ -1,6 +1,19 @@
+import {
+  InteviewSaveData,
+  ResQuestionsProps,
+} from '@/components/page/interview/result/InterviewResultContainer';
 import { instance } from '../axios';
 
 export const getInterview = async (url: string) => {
   const res = await instance(false).get(url);
   return res.data.data.result;
+};
+
+export const saveInterview = async (data: InteviewSaveData | undefined) => {
+  const res = await instance().post('/api/member/interview/save', {
+    gptOpinion: data?.gptOpinion,
+    categoryName: data?.categoryName,
+    questions: data?.questions,
+  });
+  return res;
 };
