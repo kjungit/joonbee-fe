d9468f9d28b507d94f346a66428fdf1ef29ef28d
kjun <kjun_all@naver.com>
2024-01-18 23:14:56 +0900

    Feat: ContentLayout 컴포넌트 추가

--- src/app/interview/choice/setting/page.tsx
+++ src/app/interview/choice/setting/page.tsx
@@ -1,13 +1,11 @@
 import QuestionChocieSetting from '@/components/page/interview/choice/setting/QuestionChocieSetting';
+import ContentLayout from '@/components/ui/layouts/ContentLayout';
 import React from 'react';
 
 export default function SettingPage() {
   return (
-    <div className="h-full flex justify-center items-center bg-main-primary">
-      <section className="w-[1024px] h-[600px] flex flex-col gap-4 bg-background-lightgray p-8  rounded-[40px] relative">
-        <h2 className="text-[20px] font-bold">면접 전 설정해주세요</h2>
-        <QuestionChocieSetting />
-      </section>
-    </div>
+    <ContentLayout>
+      <QuestionChocieSetting />
+    </ContentLayout>
   );
 }
