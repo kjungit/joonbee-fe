96e976d7d27429d8e01a07f34d2f1b8094a0347b
kjun <kjun_all@naver.com>
2024-01-18 23:14:11 +0900

    Feat: ContentLayout 컴포넌트 추가

--- src/app/interview/choice/page.tsx
+++ src/app/interview/choice/page.tsx
@@ -1,13 +1,11 @@
 import QuestionChoice from '@/components/page/interview/choice/QuestionChoice';
+import ContentLayout from '@/components/ui/layouts/ContentLayout';
 import React from 'react';
 
 export default function ChoicePage() {
   return (
-    <div className="h-full flex justify-center items-center bg-main-primary">
-      <section className="w-[1024px] h-[600px] flex flex-col gap-5 bg-background-lightgray p-8  rounded-[40px] relative">
-        <h2 className="text-[20px] font-bold">질문을 준비해주세요</h2>
-        <QuestionChoice />
-      </section>
-    </div>
+    <ContentLayout>
+      <QuestionChoice />
+    </ContentLayout>
   );
 }
