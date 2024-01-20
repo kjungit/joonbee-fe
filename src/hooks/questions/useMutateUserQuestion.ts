1956b994ce6beaf41cdaba18725afb176b14e2e3
dokimion24 <leecy3610@gmail.com>
2024-01-21 01:49:07 +0900

    Fix: mutate 제거

--- src/hooks/questions/useMutateUserQuestion.ts
+++ src/hooks/questions/useMutateUserQuestion.ts
@@ -9,14 +9,11 @@ export default function useMutateUserQuestion(
   questionContent: string,
   setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>,
 ) {
-  const { mutate } = useInfiniteUserQuestion(category, subcategory);
-
   const { trigger, data } = useSWRMutation(
     '/api/cart/question/save',
     () => postUserQuestion(category, subcategory, questionContent),
     {
       onSuccess: () => {
-        mutate();
         if (setIsOpen) setIsOpen(true);
       },
     },
