1ff694349b4a86fa3749a793cb95a1a732f524b6
kjun <kjun_all@naver.com>
2024-01-23 21:41:12 +0900

    Feat: images_domains 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['k.kakaocdn.net'],
+    domains: ['k.kakaocdn.net', 'phinf.pstatic.net', 'i.ytimg.com'],
   },
   async rewrites() {
     return [
