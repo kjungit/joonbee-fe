5270bf93823b9b0a10d959c98ba6be29271dc2bb
kjun <kjun_all@naver.com>
2024-01-23 21:45:52 +0900

    Feat: nave Oauth 페이지 구현

---
+++ src/app/oauth/naver/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+import React, { useEffect } from 'react';
+import useSWR from 'swr';
+import { useRecoilState } from 'recoil';
+import { isTokenedState } from '@/recoil/isTokened/atoms';
+import { useRouter, useSearchParams } from 'next/navigation';
+import Image from 'next/image';
+import { isLoginedStatus } from '@/recoil/isLogined/atom';
+import { useUserInfo } from '@/hooks/useUserInfo';
+import { naverLogin } from '@/app/apis/services/auth';
+
+export default function OauthPage() {
+  const searchParams = useSearchParams();
+  const AUTHORIZATION_CODE: string = searchParams.get('code') as string;
+  const router = useRouter();
+  const [isTokened, setIsTokened] = useRecoilState(isTokenedState);
+  const [isLogined, setisLogined] = useRecoilState(isLoginedStatus);
+  const { userInfoMutate } = useUserInfo();
+
+  const { data, error } = useSWR('/auth/naver', () => naverLogin(AUTHORIZATION_CODE), {
+    onSuccess: () => {
+      router.push('/');
+      setisLogined(true);
+      userInfoMutate();
+    },
+  });
+  useEffect(() => {
+    return () => {
+      router.push('/');
+      if (error) {
+        setIsTokened({
+          ...isTokened,
+          id: error.data,
+          isLogined: true,
+        });
+      }
+    };
+  }, [data, error]);
+  return (
+    <div className="flex items-center justify-center">
+      <Image src={'/loginLoading.gif'} width={250} height={250} alt="loding" />
+    </div>
+  );
+}
