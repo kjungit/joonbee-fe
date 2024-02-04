d3a0a4ecac3b7e5ae287b078e3324acdb382d8bd
kjun <kjun_all@naver.com>
2024-02-04 18:57:21 +0900

    Feat: 스타일 코드 수정

--- src/components/page/Main/TopSection.tsx
+++ src/components/page/Main/TopSection.tsx
@@ -1,13 +1,19 @@
+'use client';
 import { LinkBox } from '@/components/common/LinkBox';
+import { useUserInfo } from '@/hooks/useUserInfo';
 import Image from 'next/image';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
+import { Cookies } from 'react-cookie';
 
 export default function TopSection() {
+  const { isLogined, userInfo, userInfoMutate } = useUserInfo();
+
   return (
     <section className=" px-5 w-full max-w-[1024px] mainBg flex flex-col items-center min-h-[480px] ">
       <div className=" w-full flex-grow items-center">
         <p className="z-10 w-full  pt-10 font-bold flex max-w-[1024px] text-lg ">AI 면접 서비스</p>
-        <div className="flex w-full h-full max-h-[340px] md:flex-row flex-col max-w-[1024px] items-center justify-center gap-4 md:gap-10  flex-grow">
+        <div className="flex w-full h-[330px] max-h-[340px] md:flex-row flex-col max-w-[1024px] items-center justify-center gap-4 md:gap-10  flex-grow">
           <LinkBox size="md" color="navy" title="AI면접 시작하기" propsLink="/interview">
             <Image
               src="/icons/ai_white.png"
@@ -18,18 +24,24 @@ export default function TopSection() {
             />
           </LinkBox>
           <div className="flex flex-row md:flex-col gap-4 md:gap-10 w-full justify-between  flex-grow">
-            <LinkBox size="sm" title="나의 질문 관리하기" propsLink="/my">
+            <LinkBox
+              size="sm"
+              title="나의 면접 관리하기"
+              propsLink={isLogined ? '/my?category=interview&sort=my_interview' : '/login'}>
               <Image
-                src="/icons/service.png"
+                src="/icons/meeting.png"
                 alt="ai_white"
                 width={100}
                 height={100}
                 className="w-14  lg:w-20"
               />
             </LinkBox>
-            <LinkBox size="sm" title="나의 질문 확인하기" propsLink="/my">
+            <LinkBox
+              size="sm"
+              title="나의 질문 확인하기"
+              propsLink={isLogined ? '/my?category=question' : '/login'}>
               <Image
-                src="/icons/meeting.png"
+                src="/icons/service.png"
                 alt="ai_white"
                 width={100}
                 height={100}
