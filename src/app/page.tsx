a157072d4e5e8c18b4a295f63830984d1ed9d938
kjun <kjun_all@naver.com>
2024-02-08 19:20:49 +0900

    Fix: 배포 에러 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div className=" w-full flex items-center flex-col">
       <TopSection />
-      {/* <InterviewSection /> */}
-      <QuestionSection />
+      {/* <InterviewSection />
+      <QuestionSection /> */}
       <InfoSection />
     </div>
   );
