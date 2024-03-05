e5a487d684581e3c1cc02867f17b198d22eb8e4a
kjun <kjun_all@naver.com>
2024-03-05 20:45:32 +0900

    Feat: KAN-124--/로그인 페이지 작업

--- src/components/@common/SocialLoginButton/index.tsx
+++ src/components/@common/SocialLoginButton/index.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '@/utils/format';
-import { Icon } from '../../ui/Icon';
+import { Icon } from '../icon/icon';
 
-export type IconName = 'kakao' | 'naver' | 'google';
+export type IconName = 'kakao.png' | 'naver.png' | 'google.png';
 
 export type SocialLoginButtonProps = {
   name: IconName;
   onClick: () => void;
 };
 
-export const SocialLoginButton = ({ name = 'kakao', onClick }: SocialLoginButtonProps) => {
+export const SocialLoginButton = ({ name, onClick }: SocialLoginButtonProps) => {
   const socialStyles = {
-    kakao: {
+    'kakao.png': {
       boxStyle: 'bg-yellow-kakao hover:border-main-primary border-4 border-yellow-kakao',
       text: 'text-black',
     },
-    naver: {
+    'naver.png': {
       boxStyle: 'bg-green-naver hover:border-main-primary border-4 border-green-naver',
       text: 'text-white',
     },
-    google: {
+    'google.png': {
       boxStyle:
         'bg-white hover:border-main-primary hover:border-4 p-[2px] hover:p-0 border-2 border-main-primary',
       text: 'text-black',
@@ -30,10 +30,14 @@ export const SocialLoginButton = ({ name = 'kakao', onClick }: SocialLoginButton
   const capitalizedName = capitalizeFirstLetter(name);
 
   return (
-    <button className={`${boxStyle} w-[340px] h-[70px] rounded-lg `} onClick={onClick}>
+    <button className={`${boxStyle} w-[300px] h-[60px] rounded-lg `} onClick={onClick}>
       <div className="h-full px-[30px] py-[10px] flex justify-start items-center  ">
-        <Icon name={name} />
-        <h2 className={`${text} font-bold text-[16px] pl-8`}>{capitalizedName}로 시작하기</h2>
+        <div className="w-12 h-12 flex items-center justify-center">
+          <Icon name={name} />
+        </div>
+        <h2 className={`${text} font-bold text-[14px] pl-8`}>
+          {capitalizedName.slice(0, -4)}로 시작하기
+        </h2>
       </div>
     </button>
   );
--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,4 +1,4 @@
-import { IconName } from '../../public/.legacy/components/ui/Icon';
+import { IconName } from '@/components/@common/SocialLoginButton';
 
 export const capitalizeFirstLetter = (text: IconName) => {
   return text.charAt(0).toUpperCase() + text.slice(1);
