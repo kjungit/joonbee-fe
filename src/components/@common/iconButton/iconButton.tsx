d8d26deb54e1d4a87ca8720e71a92a149b444aaa
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-05 14:55:15 +0900

    Feat: IconButton 컴포넌트 생성 -컴포넌트 생성 -스토리 생성

---
+++ src/components/@common/iconButton/iconButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Button, { ButtonProps } from '../button/button';
+import { IconName, VariableIcon } from '../VariableIcon';
+
+interface IconButtonProps extends ButtonProps {
+  iconName: IconName;
+  edge?: 'start' | 'end';
+}
+
+export default function IconButton({
+  iconName,
+  children,
+  size = 'auto',
+  color = 'primary',
+  edge = 'start',
+  ...props
+}: IconButtonProps) {
+  const sizeStyles = {
+    auto: 14,
+    xs: 14,
+    sm: 14,
+    md: 14,
+    lg: 16,
+    xl: 18,
+  };
+
+  return (
+    <Button size={size} color={color} {...props}>
+      <div className="flex justify-center gap-2 items-center">
+        {edge === 'start' && <VariableIcon name={iconName} size={sizeStyles[size]} />}
+        {children}
+        {edge === 'end' && <VariableIcon name={iconName} size={sizeStyles[size]} />}
+      </div>
+    </Button>
+  );
+}
