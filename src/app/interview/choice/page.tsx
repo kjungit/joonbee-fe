a2d36c0aad1062363c7e4f7711ac19d5f79c80bc
dokimion24 <leecy3610@gmail.com>
2024-03-06 08:36:05 +0900

    Feat: 면접 페이지 추가

--- src/app/interview/choice/page.tsx
+++ src/app/interview/choice/page.tsx
@@ -1,5 +1,25 @@
+import Button from '@/components/@common/button/button';
+import { Text } from '@/components/@common/text/text';
+import Image from 'next/image';
 import React from 'react';
 
 export default function ChoicePage() {
-  return <div>ChoicePage</div>;
+  return (
+    <section className="h-[calc(100%-60px)] relative">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-5">
+        <Image src="/fluid.png" alt="랜덤면접" width={180} height={180} />
+        <div>
+          <Text size="lg" as="h4" weight="lg">
+            질문을 직접 선택해요
+          </Text>
+          <Text color="lightGray">
+            면접 질문들을 선택해서 준비할 수 있어요
+            <br />
+            체계적으로 준비해봐요!
+          </Text>
+        </div>
+        <Button size="xl">면접 시작하기</Button>
+      </div>
+    </section>
+  );
 }
--- src/app/interview/random/page.tsx
+++ src/app/interview/random/page.tsx
@@ -1,5 +1,24 @@
+import Button from '@/components/@common/button/button';
+import { Text } from '@/components/@common/text/text';
+import Image from 'next/image';
 import React from 'react';
 
 export default function RandomPage() {
-  return <div>RandomPage</div>;
+  return (
+    <section className="h-[calc(100%-60px)] relative">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-5">
+        <Image src="/rope.png" alt="랜덤면접" width={180} height={180} />
+        <div>
+          <Text size="lg" as="h4" weight="lg">
+            질문을 AI가 준비해요
+          </Text>
+          <Text color="lightGray">
+            무슨 문제가 나올지 몰라요! <br />
+            예상하지 못한 문제를 준비해보세요!
+          </Text>
+        </div>
+        <Button size="xl">면접 시작하기</Button>
+      </div>
+    </section>
+  );
 }
