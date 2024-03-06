8bdd48e7dee62c5410c5dfb3e5b213fabb0b7221
dokimion24 <leecy3610@gmail.com>
2024-03-07 08:50:36 +0900

    Feat: InterviewTypeMenu 추가

---
+++ src/components/@common/interviewTypeMenu/interviewTypeMenu.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import IconButton from '@/components/@common/iconButton/iconButton';
+
+export default function InterviewTypeMenu() {
+  const pathName = usePathname();
+
+  return (
+    <div className="flex items-center justify-between w-full px-4">
+      <div className="flex gap-4">
+        <Link href="/interview/choice">
+          <IconButton
+            iconName="choice"
+            color="white"
+            size="sm"
+            className={`${pathName.includes('choice') ? 'font-bold bg-blue-light' : ''}`}>
+            선택 질문
+          </IconButton>
+        </Link>
+        <Link href="/interview/random">
+          <IconButton
+            iconName="random"
+            color="white"
+            size="sm"
+            className={`${pathName.includes('random') ? 'font-bold bg-blue-light' : ''}`}>
+            랜덤 질문
+          </IconButton>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/components/header/secondHeader.tsx
+++ src/components/header/secondHeader.tsx
@@ -1,28 +1,14 @@
 'use client';
 import React from 'react';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import InterviewTypeMenu from '@/components/@common/interviewTypeMenu/interviewTypeMenu';
 
 export default function SecondHeader() {
   const pathName = usePathname();
+
   return (
-    <header
-      className="w-full
-    h-[60px] effect-white dark:effect-dark dark:border-b  flex items-center ">
-      <div className="flex items-center justify-between w-full px-4">
-        <div className="flex gap-4">
-          <Link
-            className={`${pathName.includes('choice') && 'font-bold'}`}
-            href="/interview/choice">
-            선택 질문
-          </Link>
-          <Link
-            className={`${pathName.includes('random') && 'font-bold'}`}
-            href="/interview/random">
-            랜덤 질문
-          </Link>
-        </div>
-      </div>
+    <header className="w-full h-[60px] effect-white dark:effect-dark dark:border-b  flex items-center ">
+      <InterviewTypeMenu />
     </header>
   );
 }
