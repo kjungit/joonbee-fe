bced61dd7a4fe21800bbd14cf1b66385829c9b08
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-08 14:39:01 +0900

    Feat: TextArea 컴포넌트 생성 -컴포넌트 생성 -스토리 생성

---
+++ src/components/@common/textArea/textArea.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import TextArea from './textArea';
+
+const meta: Meta<typeof TextArea> = {
+  title: 'Design System/Atoms/TextArea',
+  component: TextArea,
+  tags: ['autodocs'],
+  argTypes: {
+    className: {
+      control: 'text',
+    },
+    readOnly: {
+      control: 'boolean',
+    },
+  },
+  args: {
+    color: 'active',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof TextArea>;
+
+export const Default: Story = {};
---
+++ src/components/@common/textArea/textArea.tsx
@@ -0,0 +1,20 @@
+import React, { TextareaHTMLAttributes } from 'react';
+
+export interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  className?: string;
+  readOnly?: boolean;
+}
+
+export default function TextArea({ readOnly, className, ...props }: TextAreaProps) {
+  const readOnlyStyles = readOnly ? 'bg-[#F8F9Fa] text-gray-disabled' : 'focus:border-blue-light';
+
+  return (
+    <textarea
+      rows={8}
+      cols={80}
+      readOnly={readOnly}
+      className={`${readOnlyStyles} py-2 px-3 rounded-md border border-gray-normal ${className}`}
+      {...props}
+    />
+  );
+}
