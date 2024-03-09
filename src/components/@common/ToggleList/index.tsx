ee3bf5ed4b4fdb6fc2a0ec83bd377fbdbc10c158
kjun <kjun_all@naver.com>
2024-03-10 02:52:09 +0900

    Feat: ToggleList 컴포넌트 구현

--- src/components/@common/ToggleList/index.tsx
+++ src/components/@common/ToggleList/index.tsx
@@ -1,85 +1,111 @@
 'use client';
-import React, { useState } from 'react';
-import { ItemProps, ToggleItem } from '../toggleItem/toggleItem';
-// export const questionCategory: QuestionCategory[] = [
-//   { category: 'fe', subcategory: ['react', 'vuejs', 'nextjs'] },
-//   { category: 'be', subcategory: ['db', 'spring_framework', 'msa'] },
-//   { category: 'language', subcategory: ['javascript', 'typescript'] },
-//   { category: 'cs', subcategory: ['docker', 'operatingSystem', 'computerArchitecture'] },
-//   { category: 'mobile', subcategory: ['aos', 'ios', 'flutter'] },
-//   { category: 'etc', subcategory: ['git'] },
-// ];
-const data = [
-  {
-    id: 'fe',
-    value: '프론트엔드',
-    isOpen: false,
-    children: [
-      { id: 'react', value: '리액트' },
-      { id: 'vuejs', value: 'VUE' },
-      { id: 'nextjs', value: 'NEXT.js' },
-    ],
-  },
-  {
-    id: 'backend',
-    isOpen: false,
-    value: '백엔드',
-    children: [
-      { id: 'html', value: 'HTML' },
-      { id: 'css', value: 'CSS' },
-      { id: 'javascript', value: 'JAVASCRIPT' },
-    ],
-  },
-  {
-    id: 'language',
-    isOpen: false,
-    value: '언어',
-    children: [
-      { id: 'java', value: 'JAVA' },
-      { id: 'css', value: 'CSS' },
-      { id: 'javascript', value: 'JAVASCRIPT' },
-    ],
-  },
-  {
-    id: 'ect',
-    isOpen: false,
-    value: '기타',
-    children: [
-      { id: 'html', value: 'HTML' },
-      { id: 'css', value: 'CSS' },
-      { id: 'javascript', value: 'JAVASCRIPT' },
-    ],
-  },
-];
-export const ToggleItemList = () => {
-  const [items, setItems] = useState<ItemProps[]>(data);
+import React, { useEffect, useState } from 'react';
+import { ToggleItem } from '../toggleItem/toggleItem';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { selectMenuState } from '@/recoils/home/selectMenu/atom';
+import { Text } from '../text/text';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { toggleNavbarQuestionList } from '@/constants/toggleNavbarItem';
+import { InterviewMenu } from '@/components/navbar/interviewMenu';
+import { CategoryName, ChildrenProps, SubcategoryName, ToggleItemProps } from '@/types';
+import { selectQuestionCategoryState } from '@/recoils/home/question/selectQuestionCategory/atom';
 
-  const onClickOpen = (clickedItem: ItemProps) => {
+export const ToggleItemList = () => {
+  const [items, setItems] = useState<ToggleItemProps[]>(toggleNavbarQuestionList);
+  const [selectQuestionCategory, setSelectQuestionCategory] = useRecoilState(
+    selectQuestionCategoryState,
+  );
+
+  const selectMenu = useRecoilValue(selectMenuState);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const fieldParams = searchParams.get('Qfield') as CategoryName;
+  const subFieldParams = searchParams.get('subField') as SubcategoryName;
+  const categoryParams = searchParams.get('category');
+
+  const handleClickOpen = (clickedItem: ToggleItemProps) => {
+    const foundItem = items.find(item => item.id === clickedItem.id);
+
+    if (foundItem && foundItem.isOpen) {
+      return;
+    }
     const updatedItems = items.map(item => {
-      console.log(item.id === clickedItem.id);
       if (item.id === clickedItem.id) {
         return { ...item, isOpen: !item.isOpen };
       }
-      return item;
+      return { ...item, isOpen: false };
+    });
+    setSelectQuestionCategory({
+      category: clickedItem.id,
+      subCategory: clickedItem.children[0].id,
     });
     setItems(updatedItems);
   };
 
+  const handleClickChild = (childItem: ChildrenProps) => {
+    setSelectQuestionCategory({
+      ...selectQuestionCategory,
+      subCategory: childItem.id,
+    });
+  };
+
+  useEffect(() => {
+    setSelectQuestionCategory({
+      category: fieldParams || 'fe',
+      subCategory: subFieldParams || 'react',
+    });
+  }, [selectMenu]);
+
+  useEffect(() => {
+    console.log(fieldParams, subFieldParams);
+  }, []);
+
   return (
-    <div>
-      {items.map(item => (
-        <div key={item.id} className={`toggle-item ${item.isOpen ? 'open' : ''}`}>
-          <ToggleItem item={item} onClickOpen={onClickOpen} />
-          <div className="ml-10">
-            {item.isOpen &&
-              item.children?.map(childItem => (
-                <div key={childItem.id}>
-                  <button> {childItem.value}</button>
+    <div className="p-4">
+      <div>
+        {categoryParams === 'interview' ? (
+          <InterviewMenu />
+        ) : (
+          <div>
+            <Text size="xl" weight="lg">
+              질문 보기
+            </Text>
+            {items.map(item => (
+              <div
+                key={item.id}
+                className={`toggle-item ${fieldParams === item.id && 'font-bold'}`}>
+                <ToggleItem
+                  item={item}
+                  onClickOpen={() => {
+                    if (item.id === fieldParams) return;
+                    handleClickOpen(item);
+                    router.push(
+                      `/?category=${categoryParams}&Qfield=${item.id}&subField=${item.children[0].id}`,
+                    );
+                  }}
+                />
+                <div className="font-normal flex flex-col text-start w-full gap-2">
+                  {fieldParams === item.id &&
+                    item.children?.map(childItem => (
+                      <button
+                        key={childItem.id}
+                        onClick={() => {
+                          handleClickChild(childItem);
+                          router.push(
+                            `/?category=${categoryParams}&Qfield=${item.id}&subField=${childItem.id}`,
+                          );
+                        }}>
+                        <div className={`${subFieldParams === childItem.id && 'font-bold'}`}>
+                          {childItem.value}
+                        </div>
+                      </button>
+                    ))}
                 </div>
-              ))}
+              </div>
+            ))}
           </div>
-        </div>
-      ))}
+        )}
+      </div>
     </div>
   );
 };
