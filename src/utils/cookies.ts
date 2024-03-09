63030c55c1b7c149a779a0b32fdfce6a20690551
kjun <kjun_all@naver.com>
2024-03-10 02:54:54 +0900

    Feat: utils 함수 추가

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -8,4 +8,10 @@ export const getCookie = (key: string) => {
   return cookies.get(key);
 };
 
-export const setCookie = () => {};
+export const setCookie = (key: string, value: any, options?: any) => {
+  cookies.set(key, value, options);
+};
+
+export const removeCookie = (key: string) => {
+  cookies.remove(key);
+};
