74d504c5f3474e135109751fa12414c54691fb8d
kjun <kjun_all@naver.com>
2024-03-10 02:50:06 +0900

    Feat: interview layout 추가

--- src/app/interview/layout.tsx
+++ src/app/interview/layout.tsx
@@ -1,13 +1,5 @@
-import SecondHeader from '@/components/header/secondHeader';
-import Link from 'next/link';
-
 export const dynamic = 'force-dynamic';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="w-full">
-      <SecondHeader />
-      {children}
-    </div>
-  );
+  return <div className="w-full">{children}</div>;
 }
