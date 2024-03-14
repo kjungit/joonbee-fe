2e3a3462713a45f51a11b73d98111b76f661260f
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-14 18:59:06 +0900

    Feat: interview 관련 atom 추가

--- src/recoils/interview/atom.ts
+++ src/recoils/interview/atom.ts
@@ -1,4 +1,4 @@
-import { CategoryName } from '@/types';
+import { CategoryName, InterviewType } from '@/types';
 import { atom } from 'recoil';
 
 export const InterviewCategoryAtom = atom<CategoryName>({
@@ -6,11 +6,21 @@ export const InterviewCategoryAtom = atom<CategoryName>({
   default: 'fe',
 });
 
-export const interviewQuestionCountAtom = atom({
+export const interviewQuestionCountAtom = atom<number>({
   key: 'interviewQuestionCountAtom',
   default: 2,
 });
-export const interviewTimeAtom = atom({
+export const interviewTimeAtom = atom<number>({
   key: 'interviewTimeAtom',
   default: 60,
+});
+
+export const interviewVideoUrlAtom = atom<string>({
+  key: 'interviewVideoUrlAtom',
+  default: '',
+});
+
+export const interviewTypeAtom = atom<InterviewType>({
+  key: 'interviewTypeAtom',
+  default: 'random',
 });
