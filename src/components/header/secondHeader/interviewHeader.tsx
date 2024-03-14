e0aef656a666ee2ed6138e9558c7bf48c6df2011
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-14 19:00:01 +0900

    Feat: 버튼 아이콘 추가

--- src/components/header/secondHeader/interviewHeader.tsx
+++ src/components/header/secondHeader/interviewHeader.tsx
@@ -1,3 +1,4 @@
+import IconButton from '@/components/@common/iconButton/iconButton';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
 
@@ -5,13 +6,27 @@ export const InterviewHeader = () => {
   const pathName = usePathname();
 
   return (
-    <>
-      <Link className={`${pathName.includes('choice') && 'font-bold'}`} href="/interview/choice">
-        선택 질문
-      </Link>
-      <Link className={`${pathName.includes('random') && 'font-bold'}`} href="/interview/random">
-        랜덤 질문
-      </Link>
-    </>
+    <nav className="flex items-center justify-between w-full px-4 relative">
+      <div className="flex gap-4">
+        <Link href="/interview/choice">
+          <IconButton
+            iconName="checklist.png"
+            color="white"
+            size="sm"
+            className={`${pathName.includes('choice') ? 'font-bold bg-blue-light' : ''}`}>
+            선택 질문
+          </IconButton>
+        </Link>
+        <Link href="/interview/random">
+          <IconButton
+            iconName="random.png"
+            color="white"
+            size="sm"
+            className={`${pathName.includes('random') ? 'font-bold bg-blue-light' : ''}`}>
+            랜덤 질문
+          </IconButton>
+        </Link>
+      </div>
+    </nav>
   );
 };
