4e73b8ef6fe4d1735c90be194c62a7c055f2eab7
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-14 18:58:21 +0900

    Feat: convertSecondsToMinutes 함수 추가

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -12,3 +12,9 @@ export const maskNickname = (nickname: string) => {
   const maskedText = new Array(nickname.length - 3).fill('*');
   return visibleText + maskedText.join('');
 };
+
+export const convertSecondsToMinutes = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const renmaingSeconds = seconds % 60;
+  return renmaingSeconds === 0 ? minutes + '분 ' : minutes + '분 ' + renmaingSeconds + '초';
+};
