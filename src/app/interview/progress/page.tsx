0ddeea4d9c3b09f3c78f1d82ac6d95b82933a856
dokimion24 <leecy3610@gmail.com>
2024-03-18 08:58:41 +0900

    Design: 페이지 레이아웃 수정

--- src/app/interview/progress/page.tsx
+++ src/app/interview/progress/page.tsx
@@ -9,6 +9,8 @@ import useVideo from '@/hooks/interview/useVideo';
 import Button from '@/components/@common/button/button';
 import useTimer from '@/hooks/interview/useTimer';
 import { ProgressStatus } from '@/types';
+import TextArea from '@/components/@common/textArea/textArea';
+import { VariableIcon } from '@/components/@common/variableIcon/variableIcon';
 
 export default function ProgressPage() {
   const [currentCount, setCurrentCount] = useState<number>(1);
@@ -17,6 +19,8 @@ export default function ProgressPage() {
   const { questionData } = useGetInterviewData();
   const { videoRef, onStartVideo, onStartRecord, onStopRecord, onToggleRecord } = useVideo();
   const { remainingTime } = useTimer(progressStatus, setProgressStatus);
+
+  console.log(questionData);
 
   useEffect(() => {
     onStartVideo();
@@ -27,18 +31,26 @@ export default function ProgressPage() {
       <Text size="xl" className="mb-5">
         질문 {currentCount}
       </Text>
-      <div>
+      <div className="flex gap-10 mb-10">
         <Video videoRef={videoRef} />
-        <div>
-          <div className="bg-blue-light rounded-md w-[230px] h-[48px]">
-            <div className="flex items-center gap-5">
-              <Text size="lg">남은시간</Text>
-              <Text size="xl">{remainingTime}</Text>
+        <div className="flex flex-col justify-between">
+          <div className="bg-blue-light rounded-md w-[234px] h-[48px] flex justify-center">
+            <div className="flex items-center gap-5 ">
+              <Text className="text-[18px]">남은시간</Text>
+              <Text weight="lg" className="text-[18px]">
+                {remainingTime}
+              </Text>
             </div>
           </div>
           <Button size="xl">시작하기</Button>
         </div>
       </div>
+      <div className="flex items-center mb-2">
+        <VariableIcon name="tringleRight" />
+        <Text size="lg">질문 {currentCount}</Text>
+      </div>
+
+      <TextArea />
     </>
   );
 }
