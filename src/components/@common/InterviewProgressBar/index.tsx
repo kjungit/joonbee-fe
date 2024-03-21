334e767e98f2310f0483078e0edd6f7ed8d5b814
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-21 19:23:14 +0900

    Feat: 레이아웃 수정

--- src/components/@common/InterviewProgressBar/index.tsx
+++ src/components/@common/InterviewProgressBar/index.tsx
@@ -10,64 +10,65 @@ type InterivewProgress =
   | 'InterviewResult';
 
 interface InterviewProgressBar {
-  interview: InterivewProgress;
-  text: string;c
+  interview?: InterivewProgress;
+  className?: string;
 }
 
 export default function InterviewProgressBar({
   interview = 'InterviewSetting',
+  className,
 }: InterviewProgressBar) {
   return (
-    <div className="flex gap-1 items-center">
+    <div className={`flex gap-2 items-center ${className}`}>
       {interview === 'InterviewSetting' && (
         <>
           <StepIcon progress="DONE" />
-          <div className={`w-20 border-t border-[#4F7EEC] h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="PROGRESS" count={2} text="면접 설정" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-gray-normal`}></div>
           <StepIcon progress="READY" count={3} text="장치 설정" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-gray-normal`}></div>
           <StepIcon progress="READY" count={4} text="면접 진행" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-gray-normal`}></div>
           <StepIcon progress="READY" count={5} text="면접 결과" />
         </>
       )}
       {interview === 'DeviceSetting' && (
         <>
           <StepIcon progress="DONE" />
-          <div className={`w-20 border-t border-[#4F7EEC] h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="DONE" count={2} text="면접 설정" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="PROGRESS" count={3} text="장치 설정" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-gray-normal`}></div>
           <StepIcon progress="READY" count={4} text="면접 진행" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-gray-normal`}></div>
           <StepIcon progress="READY" count={5} text="면접 결과" />
         </>
       )}
       {interview === 'InterviewConducting' && (
         <>
           <StepIcon progress="DONE" />
-          <div className={`w-20 border-t border-[#4F7EEC] h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="DONE" count={2} text="면접 설정" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="DONE" count={3} text="장치 설정" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="PROGRESS" count={4} text="면접 진행" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-gray-normal`}></div>
           <StepIcon progress="READY" count={5} text="면접 결과" />
         </>
       )}
       {interview === 'InterviewResult' && (
         <>
           <StepIcon progress="DONE" />
-          <div className={`w-20 border-t border-[#4F7EEC] h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="DONE" count={2} text="면접 설정" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="DONE" count={3} text="장치 설정" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="DONE" count={4} text="면접 진행" />
-          <div className={`w-20 border-t border-gray-normal h-1`}></div>
+          <div className={`w-20 border-t-2 border-[#4F7EEC]`}></div>
           <StepIcon progress="PROGRESS" count={5} text="면접 결과" />
         </>
       )}
