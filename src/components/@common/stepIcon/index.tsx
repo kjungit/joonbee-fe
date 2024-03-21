633f794b52e3e1886fe82cd09a67822a8ff2e97b
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-21 19:24:00 +0900

    Design: 레이아웃 수정

--- src/components/@common/stepIcon/index.tsx
+++ src/components/@common/stepIcon/index.tsx
@@ -34,9 +34,9 @@ interface StepIconProps {
 export default function StepIcon({ progress, text = '면접 시작', count = 1 }: StepIconProps) {
   const { color, isIcon, backgroundColor, fontWeight } = PROGRESS_STATUS[progress];
   return (
-    <div className="inline-flex items-center">
+    <div className="inline-flex items-center mt-3">
       <div className="flex flex-col items-center gap-0.5">
-        <div className={`w-4 h-4 rounded-full ${backgroundColor}`}>
+        <div className={`w-4 h-4 rounded-full ${backgroundColor} relative`}>
           {isIcon ? (
             <Icon name="check.png" />
           ) : (
@@ -44,12 +44,12 @@ export default function StepIcon({ progress, text = '면접 시작', count = 1 }
               weight={fontWeight}
               size="sm"
               color={color}
-              className="flex justify-center itemes-center">
+              className="flex justify-center itemes-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
               {count}
             </Text>
           )}
         </div>
-        <Text weight={fontWeight} size="xs" color={color}>
+        <Text weight={fontWeight} size="md" color={color}>
           {text}
         </Text>
       </div>
