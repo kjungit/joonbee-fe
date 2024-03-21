45d72fda6f96693821cf25beee1141dba4593dca
kjun <kjun_all@naver.com>
2024-03-21 22:39:08 +0900

    Feat: useUserInfo 커스텀 훅 로직 수정

--- src/queries/user/useUserInfo.ts
+++ src/queries/user/useUserInfo.ts
@@ -1,24 +1,41 @@
 import { getUserInfo } from '@/apis/services/memberApis';
-import { isLoginedStatus } from '@/recoils/user/isLogined/atom';
-import { userInfoState } from '@/recoils/user/userInfo/atom';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { isLoginedAtom } from '@/recoils/user/isLogined/atom';
+import { NickNameAtom } from '@/recoils/user/isNickName/atom';
+import { userInfoAtom } from '@/recoils/user/userInfo/atom';
+import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 
 export const useUserInfo = () => {
-  const [userInfo, setUserInfo] = useRecoilState(userInfoState);
-  const [isLogined, setIsLogined] = useRecoilState(isLoginedStatus);
+  const [userInfo, setUserInfo] = useRecoilState(userInfoAtom);
+  const [isLogined, setIsLogined] = useRecoilState(isLoginedAtom);
+  const [nickState, setNickState] = useRecoilState(NickNameAtom);
+
   const router = useRouter();
 
-  const { mutate: userInfoMutate } = useMutation({
-    mutationKey: ['userInfo'],
-    mutationFn: getUserInfo,
-    onSuccess: data => {
-      setUserInfo(data);
-      setIsLogined(true);
-      router.push('/');
-    },
+  const {
+    data,
+    isSuccess,
+    isFetched,
+    refetch: userInfoRefetch,
+  } = useQuery({
+    queryKey: ['userInfo'],
+    queryFn: getUserInfo,
+    enabled: false,
   });
 
-  return { userInfoMutate };
+  useEffect(() => {
+    if (isSuccess) {
+      setUserInfo(data);
+      setIsLogined(true);
+      setNickState({
+        ...nickState,
+        id: data.id,
+      });
+      router.push('/');
+    }
+  }, [isSuccess, isFetched]);
+
+  return { userInfoRefetch };
 };
