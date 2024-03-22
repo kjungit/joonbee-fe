3fbf11155bfc9c03ff2a7783736259d1359d9548
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-22 19:14:42 +0900

    Feat: 선택 면접일 경우 로직 추가

--- src/hooks/interview/useGetInterviewData.ts
+++ src/hooks/interview/useGetInterviewData.ts
@@ -2,13 +2,16 @@ import { MyInterviewQuestions } from '@/apis/services/openAiApis';
 import useGetRandomQuestionList from '@/queries/question/useGetRandomQuestion';
 import { mySelectQuestionCategoryState } from '@/recoils/home/question/mySelectQuestionCategory/atom';
 import { interviewQuestionCountAtom, interviewTypeAtom } from '@/recoils/interview/atom';
+import { myInterviewAtom } from '@/recoils/myInterview/atom';
+import { addQuestionListSelector, addQuestionSelector } from '@/recoils/myInterview/withAdd';
 import { useEffect, useState } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 export default function useGetInterviewData() {
   const [questionData, setQuestionData] = useState<MyInterviewQuestions[]>([]);
   const interviewType = useRecoilValue(interviewTypeAtom);
   const mySelectCategory = useRecoilValue(mySelectQuestionCategoryState);
+  const chocieQuestionData = useRecoilValue(addQuestionSelector);
 
   const [questionCount, setQuestionCount] = useRecoilState(interviewQuestionCountAtom);
 
@@ -19,38 +22,24 @@ export default function useGetInterviewData() {
     interviewType,
   });
 
-  /* choice
-    const questions = useRecoilValue(myQuestionAtom);
-
-    const transformedQuestion = questions.map(question => ({
-      questionId: question.questionId,
-      questionContent: question.questionContent,
-      answerContent: '',
-    }));
-    */
-  /* random 
-  const category = useRecoilValue(selectedRandomCategoryAtom);
-  // const subcategory = useRecoilValue(selectedRandomSubcategoryAtom);
-  const subcategory = '';
-  const questionCount = useRecoilValue(questionCountAtom);
-
-  console.log(category, subcategory, questionCount);
-
-  const { data: questions, isLoading } = useSWR(
-    ['/api/question/gpt', category, subcategory, questionCount],
-    () => getRandomQuestions(category, subcategory, questionCount),
-  );
-
-  if (isLoading) return;
-*/
-
   useEffect(() => {
-    if (isSuccess && randomQuestionData?.length) {
-      const updatedQuestionData = randomQuestionData.map((question: any) => ({
+    if (interviewType === 'random') {
+      if (isSuccess && randomQuestionData?.length) {
+        const updatedQuestionData = randomQuestionData.map((question: any) => ({
+          ...question,
+          answerContent: '',
+        }));
+
+        setQuestionData(updatedQuestionData);
+      }
+    }
+    if (interviewType === 'choice') {
+      const updatedQuestionData = chocieQuestionData.map((question: any) => ({
         ...question,
         answerContent: '',
       }));
-      if (interviewType === 'random') setQuestionData(updatedQuestionData);
+
+      setQuestionData(updatedQuestionData);
     }
   }, [isSuccess]);
 
