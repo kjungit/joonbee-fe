376e17a274a64316956c9c462be73da0e1169915
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-22 14:55:40 +0900

    Feat: 선택면접설정 페이지 추가

--- src/app/interview/choice/setting/page.tsx
+++ src/app/interview/choice/setting/page.tsx
@@ -1,123 +1,131 @@
 'use client';
 
-import Button from '@/components/@common/button';
-import Dropdown from '@/components/@common/dropdown';
-import IconButton from '@/components/@common/iconButton';
-import { mainCategoryList } from '@/constants/category';
-import {
-  interviewChoiceCategoryAtom,
-  interviewQuestionCountAtom,
-  interviewTimeAtom,
-} from '@/recoils/interview/atom';
 import { Text } from '@/components/@common/text';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import InterviewLoading from '@/components/@common/interviewLoading/';
 import useRedirectButtonClick from '@/hooks/interview/useRedirectButtonClick';
 import Image from 'next/image';
+import { useGetMyQuestion } from '@/queries/question/useGetMyQuestion';
+import QuestionCreateForm from '@/components/@pages/interview/choice/questionCreateForm';
+import IconButton from '@/components/@common/iconButton';
+import QuestionCheck from '@/components/@pages/interview/choice/questionCheck';
+import CategoryDropdown from '@/components/@common/categoryDropdown';
+import Dropdown from '@/components/@common/dropdown';
+import { mySelectQuestionCategoryState } from '@/recoils/home/question/mySelectQuestionCategory/atom';
+import QuestionTimeButtonGroup from '@/components/@common/questionTimeButtonGroup';
 
 export default function ChoiceSettingPage() {
-  const [selected, setSelected] = useRecoilState(interviewChoiceCategoryAtom);
-  const [questionCount, setQuestionCount] = useRecoilState(interviewQuestionCountAtom);
-  const [time, setTime] = useRecoilState(interviewTimeAtom);
+  const [isClickNextBtn, setIsClickNextBtn] = useState<boolean>(false);
+  const [checkedQuestionList, setCheckedQuestionList] = useState<number[]>([]);
+  // const [selectedCategory, setSelectedCategory] = useRecoilState(selectedChoiceCategoryAtom);
+  // const [selectedSubcategory, setSelectedSubcategory] = useRecoilState(
+  //   selectedChoiceSubcategoryAtom,
+  // );
+
+  const [mySelectCategory, setMySelectCategory] = useRecoilState(mySelectQuestionCategoryState);
+
   const { onMovePage, isPressedBtn } = useRedirectButtonClick('/interview/permission');
+  const { questionData, setTarget } = useGetMyQuestion();
+
+  const handleClickQuestion = (questionId: number) => {
+    setCheckedQuestionList(prevList => {
+      if (prevList.includes(questionId)) {
+        return prevList.filter(id => id !== questionId);
+      } else {
+        return [...prevList, questionId];
+      }
+    });
+  };
+
+  const categoryList = [...new Set(questionData?.map(question => question.category))];
+
+  console.log('categoryList', categoryList);
 
   return (
     <>
       {!isPressedBtn ? (
-        <>
-          <div className="mb-5">
+        !isClickNextBtn ? (
+          <>
+            <ul className=" flex flex-col gap-4 overflow-auto h-[600px]">
+              {questionData?.map(item => (
+                <QuestionCheck
+                  key={item.questionId}
+                  question={item}
+                  isChecked={checkedQuestionList.includes(item.questionId)}
+                  onCheckChange={() => handleClickQuestion(item.questionId)}
+                />
+              ))}
+              <div ref={setTarget}></div>
+            </ul>
+            <CategoryDropdown
+              selectedCategory={mySelectCategory.category}
+              setSelectedCategory={(category: any) =>
+                setMySelectCategory(prev => ({ ...prev, category }))
+              }
+              selectedSubcategory={mySelectCategory.subCategory}
+              setSelectedSubcategory={(subCategory: any) =>
+                setMySelectCategory(prev => ({ ...prev, subCategory }))
+              }
+              className="mb-4"
+            />
+            <QuestionCreateForm />
+            <IconButton
+              iconName="next_arrow.png"
+              edge="end"
+              size="md"
+              className="absolute bottom-14 right-[300px]"
+              onClick={() => setIsClickNextBtn(true)}>
+              다음 단계
+            </IconButton>
+            <div className="absolute bottom-14 right-14">
+              <Image src="/laptop.png" alt="laptop" width={220} height={180} />
+            </div>
+          </>
+        ) : (
+          <>
+            <ul className=" flex flex-col gap-4 h-[600px] overflow-auto">
+              {questionData
+                ?.filter(question => checkedQuestionList.includes(question.questionId))
+                .map(item => (
+                  <QuestionCheck key={item.questionId} question={item} isChecked={true} />
+                ))}
+              <div ref={setTarget}></div>
+            </ul>
             <Text as="h3" size="lg" weight="lg" className="mb-2">
               전체 질문 카테고리를 선택해주세요
             </Text>
-            <Dropdown data={mainCategoryList} selected={selected} onSelect={setSelected} />
-          </div>
-          <div className="mb-5">
-            <Text as="h3" size="lg" weight="lg" className="mb-2">
-              질문 개수를 설정해주세요
-            </Text>
             <div className="flex gap-10">
               <p className="min-w-[142px] h-[48px] bg-main-primary text-white flex justify-center items-center rounded-md">
-                질문 수
+                카테고리
               </p>
-              <div className="flex gap-5">
-                <Button
-                  size="xs"
-                  color={questionCount === 2 ? 'primary' : 'white'}
-                  onClick={() => setQuestionCount(2)}>
-                  2
-                </Button>
-                <Button
-                  size="xs"
-                  color={questionCount === 4 ? 'primary' : 'white'}
-                  onClick={() => setQuestionCount(4)}>
-                  4
-                </Button>
-                <Button
-                  size="xs"
-                  color={questionCount === 6 ? 'primary' : 'white'}
-                  onClick={() => setQuestionCount(6)}>
-                  6
-                </Button>
-                <Button
-                  size="xs"
-                  color={questionCount === 8 ? 'primary' : 'white'}
-                  onClick={() => setQuestionCount(8)}>
-                  8
-                </Button>
-                <Button
-                  size="xs"
-                  color={questionCount === 10 ? 'primary' : 'white'}
-                  onClick={() => setQuestionCount(10)}>
-                  10
-                </Button>
-              </div>
+
+              <Dropdown
+                data={categoryList || []}
+                selected={mySelectCategory.category}
+                onSelect={category => setMySelectCategory(prev => ({ ...prev, category }))}
+              />
             </div>
-          </div>
-          <div className="mb-5">
-            <Text as="h3" size="lg" weight="lg" className="mb-2">
-              개별 질문 시간을 설정해주세요
-            </Text>
-            <div className="flex gap-10">
-              <p className="min-w-[142px] h-[48px] bg-main-primary text-white flex justify-center items-center rounded-md">
-                질문 시간
-              </p>
-              <div className="flex gap-5">
-                <Button
-                  size="xs"
-                  color={time === 60 ? 'primary' : 'white'}
-                  onClick={() => setTime(60)}>
-                  1분
-                </Button>
-                <Button
-                  size="xs"
-                  color={time === 90 ? 'primary' : 'white'}
-                  onClick={() => setTime(90)}>
-                  1분 30초
-                </Button>
-                <Button
-                  size="xs"
-                  color={time === 120 ? 'primary' : 'white'}
-                  onClick={() => setTime(120)}>
-                  2분
-                </Button>
-              </div>
+            <QuestionTimeButtonGroup />
+
+            <IconButton
+              iconName="next_arrow.png"
+              edge="end"
+              size="md"
+              className="absolute bottom-14 right-[300px]"
+              onClick={onMovePage}>
+              다음 단계
+            </IconButton>
+            <div className="absolute bottom-14 right-14">
+              <Image src="/laptop.png" alt="laptop" width={220} height={180} />
             </div>
-          </div>
-          <IconButton
-            iconName="next_arrow.png"
-            edge="end"
-            size="md"
-            className="absolute bottom-14"
-            onClick={onMovePage}>
-            다음 단계
-          </IconButton>
-          <div className="absolute bottom-14 right-14">
-            <Image src="/laptop.png" alt="laptop" width={180} height={180} />
-          </div>
-        </>
+          </>
+        )
       ) : (
-        <InterviewLoading />
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col flex-inline items-center gap-5">
+          <InterviewLoading />
+          <Text size="xl">면접을 준비중입니다</Text>
+        </div>
       )}
     </>
   );
