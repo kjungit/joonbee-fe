caa031f69dfdb1752ca16a608f232236741dbf68
ss-davidlee <davidlee@styleseller.co.kr>
2024-03-22 19:13:17 +0900

    Feat: addQuestionListSelector 추가

--- src/recoils/myInterview/withAdd.ts
+++ src/recoils/myInterview/withAdd.ts
@@ -16,3 +16,21 @@ export const addQuestionSelector = selector({
     set(myInterviewAtom, { ...currentInterview, questions: updatedQuestions });
   },
 });
+
+export const addQuestionListSelector = selector({
+  key: 'addQuestionListSelector',
+  get: ({ get }) => {
+    const currentInterview = get(myInterviewAtom);
+    return currentInterview.questions;
+  },
+  set: ({ set }, newValue) => {
+    // newValue를 새로운 questions 배열로 처리합니다.
+    const newQuestions = newValue as any[]; // 새로운 배열의 타입에 맞게 조정하세요.
+
+    // myInterviewAtom을 업데이트하여 questions만 새로운 배열로 대체합니다.
+    set(myInterviewAtom, prevInterview => ({
+      ...prevInterview,
+      questions: newQuestions,
+    }));
+  },
+});
