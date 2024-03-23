bc9d0c38e5dbbe2ab7acf62e133996f9806a1446
dokimion24 <leecy3610@gmail.com>
2024-03-23 15:29:49 +0900

    Feat: 모달 밖 클릭 시 닫히는 훅 추가

---
+++ src/hooks/interview/useModalOutsideClick.ts
@@ -0,0 +1,20 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+export default function useModalOutsideClick<T extends HTMLElement>(onClose: () => void) {
+  const modalRef = useRef<T | null>(null);
+
+  const handleDocumentClick = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleDocumentClick);
+    return () => {
+      document.removeEventListener('mousedown', handleDocumentClick);
+    };
+  }, []);
+
+  return { modalRef };
+}
