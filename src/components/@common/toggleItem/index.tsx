9c17b13f6b3420c53367720fe89b476c7f08e6f3
kjun <kjun_all@naver.com>
2024-03-26 23:45:15 +0900

    Feat: toggleItem 컴포넌트 수정

--- src/components/@common/toggleItem/index.tsx
+++ src/components/@common/toggleItem/index.tsx
@@ -5,7 +5,7 @@ import { ToggleProps } from '@/types';
 
 export const ToggleItem = ({ item, onClickOpen }: ToggleProps) => {
   return (
-    <button className="flex items-center" onClick={() => onClickOpen(item)}>
+    <button className="flex items-center gap-2" onClick={() => onClickOpen(item)}>
       <VariableIcon name="leftArrow" size={14} className={`${item.isOpen ? 'rotate-90' : ''}`} />
       <div>{item.value}</div>
     </button>
