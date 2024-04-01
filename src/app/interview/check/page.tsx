f5aee9b8a6e7a3cc478e2de8c745293d03b2fc01
ss-davidlee <davidlee@styleseller.co.kr>
2024-04-01 18:51:15 +0900

    Feat: 면접 확인 페이지 수정

--- src/app/interview/check/page.tsx
+++ src/app/interview/check/page.tsx
@@ -4,27 +4,32 @@ import { Accordion } from '@/components/@common/accordion';
 import { EditQuestion } from '@/components/@common/editQuestion';
 import IconButton from '@/components/@common/iconButton';
 import { Text } from '@/components/@common/text';
+import { VariableIcon } from '@/components/@common/variableIcon';
 import useBeforeUnload from '@/hooks/useBeforeUnload';
 import { addQuestionListSelector } from '@/recoils/myInterview/withAdd';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 export default function CheckPage() {
   const router = useRouter();
-  const questionList = useRecoilValue(addQuestionListSelector);
-  //   const questionList = [
-  //     { questionId: 1, questionContent: 'react 질문', answerContent: 'dfsfsdf' },
-  //     { questionId: 2, questionContent: 'react 질문', answerContent: 'dfsfsdf' },
-  //   ];
+  // const questionList = useRecoilValue(addQuestionListSelector);
+  const questionList = [
+    { questionId: 1, questionContent: 'react 질문 react 질문', answerContent: 'dfsfsdf' },
+    { questionId: 2, questionContent: 'react 질문', answerContent: 'dfsfsdf' },
+    { questionId: 3, questionContent: 'react 질문', answerContent: 'dfsfsdf' },
+  ];
+  const [textareaHeight, setTextareaHeight] = useState('auto');
+
   const setQuestionList = useSetRecoilState(addQuestionListSelector);
-  const [openStates, setOpenStates] = useState(questionList.map(() => false));
+  const [openStates, setOpenStates] = useState(questionList.map(() => true));
   const [editingStates, setEditingStates] = useState(questionList.map(() => false));
   const [currentAnswers, setCurrentAnswers] = useState(
     questionList.map(question => question.answerContent),
   );
 
+  console.log('openStates', openStates);
   const handleToggle = (index: number) => {
     const updatedOpenStates = openStates.map((isOpen, i) => (i === index ? !isOpen : isOpen));
     setOpenStates(updatedOpenStates);
@@ -40,9 +45,22 @@ export default function CheckPage() {
   const handleAnswerChange = (index: number, value: string) => {
     const updatedAnswers = currentAnswers.map((content, i) => (i === index ? value : content));
     setCurrentAnswers(updatedAnswers);
+
+    requestAnimationFrame(() => {
+      const textarea = document.getElementById(`textarea-${index}`);
+      if (textarea) {
+        textarea.style.height = `${textarea.scrollHeight}px`;
+        setTextareaHeight(textarea.style.height); // 이 부분은 상태를 업데이트하는 데 필요하다면 유지
+      }
+    });
   };
 
-  const handleInputBlur = (index: number) => {
+  const handleInputBlur = (event: React.FocusEvent<HTMLTextAreaElement>, index: number) => {
+    const content = event.target.value.replace(/\s+/g, ' ');
+    const newAnswers = [...currentAnswers];
+    newAnswers[index] = content;
+    setCurrentAnswers(newAnswers);
+
     const updatedEditingStates = editingStates.map((isEditing, i) =>
       i === index ? false : isEditing,
     );
@@ -59,8 +77,6 @@ export default function CheckPage() {
     router.push('/interview/result');
   };
 
-  useBeforeUnload();
-
   return (
     <>
       <Text as="h2" size="xl" weight="lg" className="mb-5">
@@ -69,39 +85,45 @@ export default function CheckPage() {
       <Text as="h2" size="md" weight="md" color="blue" className="mb-5">
         * 내가 답변한 내용을 확인하고 수정하세요.
       </Text>
-      <ul>
+      <div>
         {questionList.map((question, index) => (
-          <Accordion
-            key={question.questionId}
-            title={question.questionContent}
-            isMain
-            isOpen={openStates[index]}
-            onClick={() => handleToggle(index)}>
-            {editingStates[index] || currentAnswers[index] === '' ? (
-              <textarea
-                value={currentAnswers[index]}
-                className="w-full resize-none border-none px-11 text-[14px]"
-                onChange={e => handleAnswerChange(index, e.target.value)}
-                onBlur={() => handleInputBlur(index)}
+          <div key={question.questionId} className={`pb-2 border-gray-normal`}>
+            <div className={`flex cursor-pointer items-center`} onClick={() => handleToggle(index)}>
+              <VariableIcon
+                name="triangleRight"
+                size={16}
+                className={`${openStates[index] && 'rotate-90'}`}
               />
-            ) : (
-              <div onClick={() => handleDoubleClick(index)}>
-                <Text size="lg" weight="md" className="px-11">
-                  {currentAnswers[index]}
-                </Text>
+              <Text size="lg" weight="md" className="p-1">
+                {question.questionContent}
+              </Text>
+            </div>
+            {openStates[index] && (
+              <div>
+                {editingStates[index] || currentAnswers[index] === '' ? (
+                  <textarea
+                    id={`textarea-${index}`}
+                    style={{ height: textareaHeight }}
+                    value={currentAnswers[index]}
+                    className="w-full resize-none border-none px-8 text-[14px] font-medium"
+                    onChange={e => handleAnswerChange(index, e.target.value)}
+                    onBlur={e => handleInputBlur(e, index)}
+                  />
+                ) : (
+                  <Text
+                    size="lg"
+                    weight="md"
+                    className="px-8 text-[14px] "
+                    onClick={() => handleDoubleClick(index)}>
+                    {currentAnswers[index]}
+                  </Text>
+                )}
               </div>
             )}
-          </Accordion>
+          </div>
         ))}
+      </div>
 
-        {/* {questionList.map(question => (
-          <EditQuestion
-            key={question.questionId}
-            title={question.questionContent}
-            question={question.answerContent}
-          />
-        ))} */}
-      </ul>
       <IconButton
         iconName="next_arrow.png"
         edge="end"
