8fcfbadc5c63f7b9a31bec187e9d7e9813c8ce6c
kjun <kjun_all@naver.com>
2024-04-02 20:39:03 +0900

    Feat: 홈화면 분기처리 수정

--- src/components/@pages/home/homeSectionWrapper.tsx
+++ src/components/@pages/home/homeSectionWrapper.tsx
@@ -3,15 +3,18 @@ import { useSearchParams } from 'next/navigation';
 import DetailInterviewSection from './interview/detailInterviewSection';
 import { InterviewWrapper } from './interview/interviewWrapper';
 import { QuestionWrapper } from './question/questionWrapper';
+import { useRecoilValue } from 'recoil';
+import { selectInterviewState } from '@/recoils/home/interview/selectInterview/atom';
 
 export const HomeSectionWrapper = () => {
   const searchParams = useSearchParams();
   const categoryParams = searchParams.get('category');
+  const selectInterview = useRecoilValue(selectInterviewState);
 
   return (
     <div className="flex h-full w-full ">
       {categoryParams !== null && categoryParams === 'interview' && <InterviewWrapper />}
-      {categoryParams === 'interview' && <DetailInterviewSection />}
+      {categoryParams !== 'question' && <DetailInterviewSection />}
       {categoryParams === 'question' && <QuestionWrapper />}
     </div>
   );
