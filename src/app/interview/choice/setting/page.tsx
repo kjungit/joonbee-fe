a909da249aec596ef245c4d752bc4649aa4baf84
kjun <kjun_all@naver.com>
2024-04-04 23:06:16 +0900

    Style: 불필요한 padding, margin 제거

--- src/app/interview/choice/setting/page.tsx
+++ src/app/interview/choice/setting/page.tsx
@@ -94,84 +94,92 @@ export default function ChoiceSettingPage() {
     <>
       {!isPressedBtn ? (
         !isClickNextBtn ? (
-          <>
-            <Text as="h2" size="xl" weight="lg" className="mb-5">
-              나의 질문
-            </Text>
-            <Text as="h2" size="md" weight="md" color="blue" className="mb-2">
-              * 면접을 진행할 질문을 선택해주세요
-            </Text>
-            <ul className=" flex flex-col gap-4 overflow-auto h-[360px] mb-8">
-              {questionData?.map(item => (
-                <QuestionCheck
-                  key={item.questionId}
-                  question={item}
-                  isChecked={checkedQuestionIdList.some(
-                    checkedItem => checkedItem.questionId === item.questionId,
-                  )}
-                  onCheckChange={() =>
-                    handleClickQuestion(item.questionId, item.questionContent, item.category)
-                  }
-                />
-              ))}
-              <div ref={setTarget}></div>
-            </ul>
-            <div className="flex justify-between">
-              <CategoryDropdown
-                selectedCategory={mySelectCategory.category}
-                setSelectedCategory={(category: any) =>
-                  setMySelectCategory(prev => ({ ...prev, category }))
-                }
-                selectedSubcategory={mySelectCategory.subCategory}
-                setSelectedSubcategory={(subCategory: any) =>
-                  setMySelectCategory(prev => ({ ...prev, subCategory }))
-                }
-                className="mb-4"
-              />
-              <IconButton
-                iconName="next_arrow.png"
-                edge="end"
-                size="md"
-                onClick={() => setIsClickNextBtn(true)}
-                disabled={!checkedQuestionIdList.length}>
-                다음 단계
-              </IconButton>
-            </div>
-            <QuestionCreateForm />
-          </>
-        ) : (
-          <>
-            <Text as="h2" size="xl" weight="lg" className="mb-5">
-              선택한 질문
-            </Text>
-            <ul className=" flex flex-col gap-4 h-[360px] overflow-auto">
-              {checkedQuestionList.map(item => (
-                <QuestionCheck key={item.questionId} question={item} isChecked={true} />
-              ))}
-              <div ref={setTarget}></div>
-            </ul>
-            <div className="mb-5">
-              <Text as="h3" size="lg" className="mb-2">
-                전체 질문 카테고리를 선택해주세요
+          <div className="py-14 flex flex-col justify-between questionListHeight">
+            <div>
+              <Text as="h2" size="xl" weight="lg" className="mb-5">
+                나의 질문
               </Text>
-              <div className="flex gap-10">
-                <p className="min-w-[120px] h-[40px] bg-main-primary text-white flex justify-center items-center rounded-md text-[14px]">
-                  카테고리
-                </p>
-                <Dropdown
-                  data={categoryList || []}
-                  selected={mySelectCategory.category}
-                  onSelect={category => setMySelectCategory(prev => ({ ...prev, category }))}
+              <Text as="h2" size="md" weight="md" color="blue" className="mb-2">
+                * 면접을 진행할 질문을 선택해주세요
+              </Text>
+              <ul className=" flex flex-col gap-4 overflow-auto h-[360px] mb-8">
+                {questionData?.map(item => (
+                  <QuestionCheck
+                    key={item.questionId}
+                    question={item}
+                    isChecked={checkedQuestionIdList.some(
+                      checkedItem => checkedItem.questionId === item.questionId,
+                    )}
+                    onCheckChange={() =>
+                      handleClickQuestion(item.questionId, item.questionContent, item.category)
+                    }
+                  />
+                ))}
+                <div ref={setTarget}></div>
+              </ul>
+            </div>
+            <div>
+              <div className="flex justify-between">
+                <CategoryDropdown
+                  selectedCategory={mySelectCategory.category}
+                  setSelectedCategory={(category: any) =>
+                    setMySelectCategory(prev => ({ ...prev, category }))
+                  }
+                  selectedSubcategory={mySelectCategory.subCategory}
+                  setSelectedSubcategory={(subCategory: any) =>
+                    setMySelectCategory(prev => ({ ...prev, subCategory }))
+                  }
+                  className="mb-4"
                 />
+                <IconButton
+                  iconName="next_arrow.png"
+                  edge="end"
+                  size="md"
+                  onClick={() => setIsClickNextBtn(true)}
+                  disabled={!checkedQuestionIdList.length}>
+                  다음 단계
+                </IconButton>
+              </div>
+              <QuestionCreateForm />
+            </div>
+          </div>
+        ) : (
+          <div className="p-14 questionListHeight flex flex-col justify-between">
+            <div>
+              <Text as="h2" size="xl" weight="lg" className="mb-5">
+                선택한 질문
+              </Text>
+              <ul className=" flex flex-col gap-4 h-[360px] overflow-auto">
+                {checkedQuestionList.map(item => (
+                  <QuestionCheck key={item.questionId} question={item} isChecked={true} />
+                ))}
+                <div ref={setTarget}></div>
+              </ul>
+            </div>
+            <div>
+              <div className="mb-5">
+                <Text as="h3" size="lg" className="mb-2">
+                  전체 질문 카테고리를 선택해주세요
+                </Text>
+                <div className="flex gap-10">
+                  <p className="min-w-[120px] h-[40px] bg-main-primary text-white flex justify-center items-center rounded-md text-[14px]">
+                    카테고리
+                  </p>
+                  <Dropdown
+                    data={categoryList || []}
+                    selected={mySelectCategory.category}
+                    onSelect={category => setMySelectCategory(prev => ({ ...prev, category }))}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-between items-end">
+                <QuestionTimeButtonGroup />
+                <IconButton iconName="next_arrow.png" edge="end" size="md" onClick={handleMove}>
+                  다음 단계
+                </IconButton>
               </div>
             </div>
-            <div className="flex justify-between items-end">
-              <QuestionTimeButtonGroup />
-              <IconButton iconName="next_arrow.png" edge="end" size="md" onClick={handleMove}>
-                다음 단계
-              </IconButton>
-            </div>
-          </>
+          </div>
         )
       ) : (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col flex-inline items-center gap-5">
