1ca4d8d6a08618ce069faef004acfadec1a4764a
kjun <kjun_all@naver.com>
2024-04-06 03:17:19 +0900

    Feat: 모달 추가

--- src/queries/question/usePostQuestionSave.ts
+++ src/queries/question/usePostQuestionSave.ts
@@ -1,12 +1,15 @@
 import { SavaQuestionProps, postSaveQuestion } from '@/apis/services/questionApis';
+import { isSaveQuestion } from '@/recoils/user/isSaveQuestion/atom';
 import { useMutation } from '@tanstack/react-query';
+import { useRecoilState } from 'recoil';
 
 export const usePostQuestionSave = ({ subcategoryName, questionContent }: SavaQuestionProps) => {
+  const [isOpen, setIsOpen] = useRecoilState(isSaveQuestion);
   const { mutate: questionSaveMutate } = useMutation({
     mutationKey: ['postInterviewLike', subcategoryName, questionContent],
     mutationFn: () => postSaveQuestion({ subcategoryName, questionContent }),
     onSuccess: () => {
-      alert('질문을 저장했습니다.');
+      setIsOpen(true);
     },
     onError: (error: number) => {
       if (error === 401) alert('로그인 후 이용해주세요.');
