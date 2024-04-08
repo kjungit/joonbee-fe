d2e78d4be2fbcb55d4024e4f4ad68464b7636c74
ss-davidlee <davidlee@styleseller.co.kr>
2024-04-08 15:20:50 +0900

    Feat: useAudio -> useAudioStream 변경

--- src/hooks/interview/useAudioStream.ts
+++ src/hooks/interview/useAudioStream.ts
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
-export default function useAudio() {
+export default function useAudioStream() {
   const [audioStream, setAudioStream] = useState<MediaStream | null>(null);
 
-  const onStartAudio = async () => {
+  const onStartAudio = useCallback(async () => {
     try {
       console.log('onStartAudio');
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -11,7 +11,7 @@ export default function useAudio() {
     } catch (error) {
       console.error('오디오 스트림을 시작하는 데 실패했습니다:', error);
     }
-  };
+  }, [setAudioStream]);
 
   return { onStartAudio, audioStream };
 }
