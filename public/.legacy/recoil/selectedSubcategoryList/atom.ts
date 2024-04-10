bea3ac2de17d7bb3d38af31bab8a8bb290b5358b
kjun <kjun_all@naver.com>
2024-04-10 21:17:09 +0900

    Delete: legacy v1코드 삭제

--- public/.legacy/recoil/selectedSubcategoryList/atom.ts
+++
@@ -1,7 +0,0 @@
-import { SubcategoryName } from '@/types/question';
-import { atom } from 'recoil';
-
-export const selectedSubcategoryListAtom = atom<SubcategoryName[]>({
-  key: 'selectedSubcategoryListAtom',
-  default: [],
-});
