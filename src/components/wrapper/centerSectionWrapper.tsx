7b57c4249d1217a2dd1ca6960360ce7c9eb01af2
kjun <kjun_all@naver.com>
2024-04-10 17:40:36 +0900

    Feat: 모바일 대응 css 추가

--- src/components/wrapper/centerSectionWrapper.tsx
+++ src/components/wrapper/centerSectionWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const CenterSectionWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center h-full">
+    <div className="flex justify-center h-full pb-20">
       <div className="md:w-[800px] h-full w-full">{children}</div>
     </div>
   );
