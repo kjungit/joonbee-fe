551874a259bcd92c62d94163171688aecf33f0f8
kjun <kjun_all@naver.com>
2024-04-14 23:18:39 +0900

    Feat: 마이페이지 cookie router 적용

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,16 +1,28 @@
 'use client';
 
+import authApis from '@/apis/services/authApis';
 import { MyInterviewSection } from '@/components/@pages/my/myInterviewSection';
 import { MyQuestionSection } from '@/components/@pages/my/myQuestionSection';
 import { NavbarIsOpenAtom } from '@/recoils/responsive/navbar/atom';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
 import { useRecoilValue } from 'recoil';
 
 export default function MyPage() {
   const isOpen = useRecoilValue(NavbarIsOpenAtom);
   const searchParams = useSearchParams();
   const categoryParams = searchParams.get('category');
+  const [cookies] = useCookies(['joonbee-token']);
+  const router = useRouter();
 
+  useEffect(() => {
+    if (!cookies['joonbee-token']) {
+      authApis.getRefresh().then(data => {
+        if (data.status !== 200) router.push('/login');
+      });
+    }
+  }, []);
   return (
     <div className={`w-full overflow-auto questionListHeight ${isOpen && 'min-w-[320px]'}`}>
       {categoryParams === 'interview' && <MyInterviewSection />}
