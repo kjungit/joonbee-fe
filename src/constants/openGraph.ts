dd2afe33037ceccac0b9492408b6f35847b6dd54
kjun <kjun_all@naver.com>
2024-04-29 00:08:19 +0900

    Feat: openGraph 상수 추가

---
+++ src/constants/openGraph.ts
@@ -0,0 +1,22 @@
+export const META = {
+  title: '개발자를 위한 AI 면접서비스',
+  siteName: 'JOONBEE || 준비',
+  description:
+    '랜덤 질문, 선택질문을 선택해서 기술면접을 준비해보세요. AI서비스로 면접에서 부족한점을 보완할 수 있어요.',
+  keyword: [
+    '준비',
+    'AI 면접',
+    '개발자 면접 준비',
+    'JOONBEE',
+    'joonbee',
+    '개발자 면접',
+    '기술면접',
+    '개발자',
+    '면접',
+    'AI',
+  ],
+  url: 'https://www.joonbee.co.kr',
+  googleVerification: process.env.NEXT_PUBLIC_GOOGLE_CONTENT,
+  naverVerification: process.env.NEXT_PUBLIC_NAVER_CONTENT,
+  ogImage: '/og_bg.jpg',
+};
