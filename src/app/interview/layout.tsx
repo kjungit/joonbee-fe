7f2d1ff64deac789d66918fd60332a7dd484995d
kjun <kjun_all@naver.com>
2024-05-19 00:44:58 +0900

    Feat: refresh 요청 로직 제거

--- src/app/interview/layout.tsx
+++ src/app/interview/layout.tsx
@@ -1,8 +1,7 @@
 'use client';
-import authApis from '@/apis/services/authApis';
 import { ViewSizeCheck } from '@/components/@common/viewSizeCheck';
 import { CenterSectionWrapper } from '@/components/wrapper/centerSectionWrapper';
-import { usePathname, useRouter, useSelectedLayoutSegments } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 
@@ -10,15 +9,13 @@ export const dynamic = 'force-dynamic';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [isMount, setIsMount] = useState(false);
-
   const [cookies] = useCookies(['joonbee-token']);
   const router = useRouter();
   const pathName = usePathname();
+
   useEffect(() => {
     if (!cookies['joonbee-token']) {
-      authApis.getRefresh().then(data => {
-        if (data.status !== 200) router.push('/login');
-      });
+      router.push('/login');
     }
   }, []);
 
