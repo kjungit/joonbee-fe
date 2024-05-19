adc8d116c13e93887c7bd979b5fdcc11fe25b2b0
kjun <kjun_all@naver.com>
2024-05-19 19:19:35 +0900

    Feat: cookies 분기 로직 제거

--- src/app/interview/layout.tsx
+++ src/app/interview/layout.tsx
@@ -1,23 +1,14 @@
 'use client';
 import { ViewSizeCheck } from '@/components/@common/viewSizeCheck';
 import { CenterSectionWrapper } from '@/components/wrapper/centerSectionWrapper';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { useCookies } from 'react-cookie';
 
 export const dynamic = 'force-dynamic';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [isMount, setIsMount] = useState(false);
-  const [cookies] = useCookies(['joonbee-token']);
-  const router = useRouter();
   const pathName = usePathname();
-
-  useEffect(() => {
-    if (!cookies['joonbee-token']) {
-      router.push('/login');
-    }
-  }, []);
 
   useEffect(() => {
     setIsMount(true);
