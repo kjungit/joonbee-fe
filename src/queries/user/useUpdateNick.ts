7f46e23e44995c29d5d6ea74e0cdd6c47597156d
kjun <kjun_all@naver.com>
2024-05-19 19:35:24 +0900

    Feat: 닉네임 변경시 이동되는 로직 제거

--- src/queries/user/useUpdateNick.ts
+++ src/queries/user/useUpdateNick.ts
@@ -1,11 +1,9 @@
 import authApis from '@/apis/services/authApis';
 import { NickNameAtom } from '@/recoils/user/isNickName/atom';
 import { useMutation } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { useUserInfo } from './useUserInfo';
 import { userInfoAtom } from '@/recoils/user/userInfo/atom';
-import { isLoginedAtom } from '@/recoils/user/isLogined/atom';
 import { isNickAtom } from '@/recoils/user/isNickOpen/atom';
 
 export const useUpdateNick = () => {
@@ -14,19 +12,12 @@ export const useUpdateNick = () => {
   const [userInfo, setUserInfo] = useRecoilState(userInfoAtom);
   const [isNickOpen, setIsNickOpen] = useRecoilState(isNickAtom);
 
-  const isLogined = useRecoilValue(isLoginedAtom);
-
-  const router = useRouter();
-
   const { mutate: nickMutate, error: nickError } = useMutation<any, number, any>({
     mutationKey: ['useUpdateNick'],
     mutationFn: () => authApis.postNickName({ id: nickState.id, nickName: nickState.nickName }),
     onSuccess: () => {
       setIsNickOpen(!isNickOpen);
       userInfoRefetch();
-      if (!isLogined) {
-        router.push('/');
-      }
       setUserInfo({ ...userInfo, nickName: nickState.nickName });
     },
   });
