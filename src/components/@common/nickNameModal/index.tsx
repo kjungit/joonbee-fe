f338c6ed67556ca93ac00e45614a01e9be253cc7
kjun <kjun_all@naver.com>
2024-05-21 23:05:56 +0900

    Feat: 닉네임 모달 생성시 스크롤 막음

--- src/components/@common/nickNameModal/index.tsx
+++ src/components/@common/nickNameModal/index.tsx
@@ -39,7 +39,7 @@ export const NickNameModal = ({ isClose = false }: { isClose?: boolean }) => {
     }
   };
   return (
-    <div className="bg-main-primary/20 fixed z-40 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 w-screen h-screen   shadow-md flex items-center justify-center">
+    <div className="bg-main-primary/20 fixed z-40 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 w-screen h-screen overscroll-none shadow-md flex items-center justify-center">
       <form
         className="relative w-[280px] h-[260px] flex items-center justify-center rounded-xl bg-white shadow-md"
         onSubmit={handleSubmit}
