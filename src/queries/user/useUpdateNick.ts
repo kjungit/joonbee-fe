f2cc6b8dd8821cec0af51e4673f3413993973335
kjun <kjun_all@naver.com>
2024-06-02 22:40:25 +0900

    Feat: 첫 로그인시 닉네임 변경 후 router 처리

--- src/queries/user/useUpdateNick.ts
+++ src/queries/user/useUpdateNick.ts
@@ -5,22 +5,34 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import { useUserInfo } from './useUserInfo';
 import { userInfoAtom } from '@/recoils/user/userInfo/atom';
 import { isNickAtom } from '@/recoils/user/isNickOpen/atom';
+import { useRouter } from 'next/navigation';
+import { isFirstLoginAtom } from '@/recoils/user/isFirstLogin/atom';
 
 export const useUpdateNick = () => {
   const nickState = useRecoilValue(NickNameAtom);
+  const [isFirstLogin, setIsFirstLogin] = useRecoilState(isFirstLoginAtom);
+
   const { userInfoRefetch } = useUserInfo();
   const [userInfo, setUserInfo] = useRecoilState(userInfoAtom);
   const [isNickOpen, setIsNickOpen] = useRecoilState(isNickAtom);
-
-  const { mutate: nickMutate, error: nickError } = useMutation<any, number, any>({
+  const router = useRouter();
+  const {
+    mutateAsync: nickMutate,
+    error: nickError,
+    isSuccess: isNickSuccess,
+  } = useMutation<any, number, any>({
     mutationKey: ['useUpdateNick'],
     mutationFn: () => authApis.postNickName({ id: nickState.id, nickName: nickState.nickName }),
     onSuccess: () => {
       setIsNickOpen(!isNickOpen);
       userInfoRefetch();
       setUserInfo({ ...userInfo, nickName: nickState.nickName });
+      if (isFirstLogin) {
+        router.push('/');
+        setIsFirstLogin(!isFirstLogin);
+      }
     },
   });
 
-  return { nickMutate, nickError };
+  return { nickMutate, nickError, isNickSuccess };
 };
---
+++ src/recoils/user/isFirstLogin/atom.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const isFirstLoginAtom = atom({
+  key: 'isFirstLoginAtom',
+  default: false,
+});
